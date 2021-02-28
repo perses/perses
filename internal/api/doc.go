ea2d24a5264a18f2c9e1e7bb43949667e20f77dd
Augustin Husson <husson.augustin@gmail.com>
2021-02-28 15:24:28 +0100

    update endpoint generation with using the shared package for the path of the api

--- internal/api/doc.go
+++ internal/api/doc.go
@@ -14,4 +14,4 @@
 package api
 
 // this file is just there to run the command generate
-//go:generate go run generate.go -package=project -plural=projects -kind=Project
+//go:generate go run generate.go -package=project -kind=Project
