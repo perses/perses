6f2403a1a344f7132c7be16c9fcc62cd7945d2c1
Augustin Husson <husson.augustin@gmail.com>
2021-03-13 18:12:43 +0100

    stabilize client unit test by implementing a wait for cleanup mecanism

--- pkg/client/api/v1/project_test.go
+++ pkg/client/api/v1/project_test.go
@@ -18,12 +18,26 @@ package v1
 import (
 	"net/http/httptest"
 	"testing"
+	"time"
 
+	projectInterface "github.com/perses/perses/internal/api/interface/v1/project"
 	"github.com/perses/perses/pkg/client/perseshttp"
 	v1 "github.com/perses/perses/pkg/model/api/v1"
 	"github.com/perses/perses/utils"
 	"github.com/stretchr/testify/assert"
 )
+
+const retry = 25
+
+func waitUntilNoProjectExists(t *testing.T, dao projectInterface.DAO) {
+	etcdNotEmpty := true
+	for i := 0; i < retry && etcdNotEmpty; i++ {
+		list, err := dao.List(&projectInterface.Query{})
+		assert.NoError(t, err)
+		etcdNotEmpty = len(list) > 0
+		time.Sleep(1 * time.Second)
+	}
+}
 
 func createClient(t *testing.T, server *httptest.Server) ClientInterface {
 	restClient, err := perseshttp.NewFromConfig(&perseshttp.RestConfigClient{
@@ -36,7 +50,7 @@ func createClient(t *testing.T, server *httptest.Server) ClientInterface {
 }
 
 func TestCreateProject(t *testing.T) {
-	project := &v1.Project{Metadata: v1.Metadata{
+	entity := &v1.Project{Metadata: v1.Metadata{
 		Kind: v1.KindProject,
 		Name: "perses",
 	}}
@@ -45,14 +59,15 @@ func TestCreateProject(t *testing.T) {
 	defer server.Close()
 	client := createClient(t, server)
 
-	object, err := client.Project().Create(project)
+	object, err := client.Project().Create(entity)
 	assert.NoError(t, err)
-	assert.Equal(t, object.Metadata.Name, project.Metadata.Name)
+	assert.Equal(t, object.Metadata.Name, entity.Metadata.Name)
 	utils.ClearAllKeys(t, persistenceManager.GetETCDClient())
+	waitUntilNoProjectExists(t, persistenceManager.GetProject())
 }
 
 func TestCreateProjectConflict(t *testing.T) {
-	project := &v1.Project{Metadata: v1.Metadata{
+	entity := &v1.Project{Metadata: v1.Metadata{
 		Kind: v1.KindProject,
 		Name: "perses",
 	}}
@@ -61,18 +76,19 @@ func TestCreateProjectConflict(t *testing.T) {
 	defer server.Close()
 	client := createClient(t, server)
 
-	object, err := client.Project().Create(project)
+	object, err := client.Project().Create(entity)
 	assert.NoError(t, err)
-	assert.Equal(t, object.Metadata.Name, project.Metadata.Name)
+	assert.Equal(t, object.Metadata.Name, entity.Metadata.Name)
 
-	_, err = client.Project().Create(project)
+	_, err = client.Project().Create(entity)
 	assert.Error(t, err)
 
 	utils.ClearAllKeys(t, persistenceManager.GetETCDClient())
+	waitUntilNoProjectExists(t, persistenceManager.GetProject())
 }
 
 func TestUpdateProject(t *testing.T) {
-	project := &v1.Project{Metadata: v1.Metadata{
+	entity := &v1.Project{Metadata: v1.Metadata{
 		Kind: v1.KindProject,
 		Name: "perses",
 	}}
@@ -81,20 +97,21 @@ func TestUpdateProject(t *testing.T) {
 	defer server.Close()
 	client := createClient(t, server)
 
-	_, err := client.Project().Create(project)
+	_, err := client.Project().Create(entity)
 	assert.NoError(t, err)
 
-	object, err := client.Project().Update(project)
+	object, err := client.Project().Update(entity)
 	assert.NoError(t, err)
 
 	// for the moment the only thing to test is that the dates are correctly updated
 	assert.True(t, object.Metadata.CreatedAt.UnixNano() < object.Metadata.UpdatedAt.UnixNano())
 
 	utils.ClearAllKeys(t, persistenceManager.GetETCDClient())
+	waitUntilNoProjectExists(t, persistenceManager.GetProject())
 }
 
 func TestGetProject(t *testing.T) {
-	project := &v1.Project{Metadata: v1.Metadata{
+	entity := &v1.Project{Metadata: v1.Metadata{
 		Kind: v1.KindProject,
 		Name: "perses",
 	}}
@@ -102,33 +119,18 @@ func TestGetProject(t *testing.T) {
 	defer server.Close()
 	client := createClient(t, server)
 
-	_, err := client.Project().Create(project)
+	_, err := client.Project().Create(entity)
 	assert.NoError(t, err)
-	object, err := client.Project().Get(project.Metadata.Name)
+	object, err := client.Project().Get(entity.Metadata.Name)
 	assert.NoError(t, err)
-	assert.Equal(t, object.Metadata.Name, project.Metadata.Name)
+	assert.Equal(t, object.Metadata.Name, entity.Metadata.Name)
 
 	utils.ClearAllKeys(t, persistenceManager.GetETCDClient())
+	waitUntilNoProjectExists(t, persistenceManager.GetProject())
 }
 
 func TestDeleteProject(t *testing.T) {
-	project := &v1.Project{Metadata: v1.Metadata{
-		Kind: v1.KindProject,
-		Name: "perses",
-	}}
-	server, _ := utils.CreateServer(t)
-	defer server.Close()
-	client := createClient(t, server)
-
-	_, err := client.Project().Create(project)
-	assert.NoError(t, err)
-
-	err = client.Project().Delete(project.Metadata.Name)
-	assert.NoError(t, err)
-}
-
-func TestListProject(t *testing.T) {
-	project := &v1.Project{Metadata: v1.Metadata{
+	entity := &v1.Project{Metadata: v1.Metadata{
 		Kind: v1.KindProject,
 		Name: "perses",
 	}}
@@ -136,11 +138,29 @@ func TestListProject(t *testing.T) {
 	defer server.Close()
 	client := createClient(t, server)
 
-	_, err := client.Project().Create(project)
+	_, err := client.Project().Create(entity)
+	assert.NoError(t, err)
+
+	err = client.Project().Delete(entity.Metadata.Name)
+	assert.NoError(t, err)
+	waitUntilNoProjectExists(t, persistenceManager.GetProject())
+}
+
+func TestListProject(t *testing.T) {
+	entity := &v1.Project{Metadata: v1.Metadata{
+		Kind: v1.KindProject,
+		Name: "perses",
+	}}
+	server, persistenceManager := utils.CreateServer(t)
+	defer server.Close()
+	client := createClient(t, server)
+
+	_, err := client.Project().Create(entity)
 	assert.NoError(t, err)
 
 	result, err := client.Project().List("")
 	assert.NoError(t, err)
 	assert.True(t, len(result) == 1)
 	utils.ClearAllKeys(t, persistenceManager.GetETCDClient())
+	waitUntilNoProjectExists(t, persistenceManager.GetProject())
 }
