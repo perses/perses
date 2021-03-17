198f4222cd428d4498d14973a4b0205c3d4c77b1
Augustin Husson <husson.augustin@gmail.com>
2021-03-17 17:44:57 +0100

    add license on all ts / css file

--- internal/api/front/perses/src/app/project/project-routing.module.ts
+++ internal/api/front/perses/src/app/project/project-routing.module.ts
@@ -1,6 +1,19 @@
-import {RouterModule, Routes} from "@angular/router";
-import {ProjectTemplateComponent} from "./project-template/project-template.component";
-import {NgModule} from "@angular/core";
+// Copyright 2021 Amadeus s.a.s
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { RouterModule, Routes } from '@angular/router';
+import { ProjectTemplateComponent } from './project-template/project-template.component';
+import { NgModule } from '@angular/core';
 
 const PROJECT_ROUTES: Routes = [
   {
@@ -13,7 +26,7 @@ const PROJECT_ROUTES: Routes = [
       }
     ]
   }
-]
+];
 
 @NgModule({
   imports: [RouterModule.forChild(PROJECT_ROUTES)],
--- internal/api/front/perses/src/app/project/prometheusrule/prometheusrule-routing.module.ts
+++ internal/api/front/perses/src/app/project/prometheusrule/prometheusrule-routing.module.ts
@@ -1,13 +1,26 @@
-import {RouterModule, Routes} from "@angular/router";
-import {PrometheusRuleListComponent} from "./prometheusrule-list/prometheusrule-list.component";
-import {NgModule} from "@angular/core";
+// Copyright 2021 Amadeus s.a.s
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { RouterModule, Routes } from '@angular/router';
+import { PrometheusRuleListComponent } from './prometheusrule-list/prometheusrule-list.component';
+import { NgModule } from '@angular/core';
 
 const PROMETHEUSRULE_ROUTES: Routes = [
   {
     path: '',
     component: PrometheusRuleListComponent
   }
-]
+];
 
 @NgModule({
   imports: [RouterModule.forChild(PROMETHEUSRULE_ROUTES)],
