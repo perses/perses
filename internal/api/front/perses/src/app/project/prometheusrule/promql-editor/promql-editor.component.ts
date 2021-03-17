acb0fab45c77cdbd856e4d642c0ac035683f1e77
Augustin Husson <husson.augustin@gmail.com>
2021-03-17 20:03:16 +0100

    set a promQL them and make the editor readonly

--- internal/api/front/perses/src/app/project/prometheusrule/promql-editor/promql-editor.component.ts
+++ internal/api/front/perses/src/app/project/prometheusrule/promql-editor/promql-editor.component.ts
@@ -16,6 +16,7 @@ import { PromQLExtension } from 'codemirror-promql';
 import { EditorView } from '@codemirror/view';
 import { EditorState } from '@codemirror/state';
 import { basicSetup } from '@codemirror/basic-setup';
+import { HighlightStyle, tags } from '@codemirror/highlight';
 
 @Component({
   selector: 'app-promql-editor',
@@ -28,19 +29,115 @@ export class PromqlEditorComponent implements OnInit, AfterViewInit {
   @Input()
   id = '';
 
+  // promQLHighlightMaterialTheme is based on the material theme defined here:
+  // https://codemirror.net/theme/material.css
+  private promQLHighlightMaterialTheme = HighlightStyle.define(
+    {
+      tag: tags.deleted,
+      textDecoration: 'line-through',
+    },
+    {
+      tag: tags.inserted,
+      textDecoration: 'underline',
+    },
+    {
+      tag: tags.link,
+      textDecoration: 'underline',
+    },
+    {
+      tag: tags.strong,
+      fontWeight: 'bold',
+    },
+    {
+      tag: tags.emphasis,
+      fontStyle: 'italic',
+    },
+    {
+      tag: tags.invalid,
+      color: '#f00',
+    },
+    {
+      tag: tags.keyword,
+      color: '#C792EA',
+    },
+    {
+      tag: tags.operator,
+      color: '#89DDFF',
+    },
+    {
+      tag: tags.atom,
+      color: '#F78C6C',
+    },
+    {
+      tag: tags.number,
+      color: '#FF5370',
+    },
+    {
+      tag: tags.string,
+      color: '#99b867',
+    },
+    {
+      tag: [tags.escape, tags.regexp],
+      color: '#e40',
+    },
+    {
+      tag: tags.definition(tags.variableName),
+      color: '#f07178',
+    },
+    {
+      tag: tags.labelName,
+      color: '#f07178',
+    },
+    {
+      tag: tags.typeName,
+      color: '#085',
+    },
+    {
+      tag: tags.function(tags.variableName),
+      color: '#C792EA',
+    },
+    {
+      tag: tags.definition(tags.propertyName),
+      color: '#00c',
+    },
+    {
+      tag: tags.comment,
+      color: '#546e7a',
+    }
+  );
+
+  private customTheme = EditorView.theme({
+    $completionDetail: {
+      marginLeft: '0.5em',
+      float: 'right',
+      color: '#9d4040',
+    },
+    $completionMatchedText: {
+      color: '#83080a',
+      textDecoration: 'none',
+      fontWeight: 'bold',
+    },
+  });
+
   constructor() {
   }
 
   ngOnInit(): void {
 
   }
+
   ngAfterViewInit(): void {
     const doc = document.getElementById(`${this.id}`);
     if (doc !== null) {
       const promQLExtension = new PromQLExtension();
       const view = new EditorView({
         state: EditorState.create({
-          extensions: [basicSetup, promQLExtension.asExtension()],
+          extensions: [
+            basicSetup,
+            promQLExtension.asExtension(),
+            this.promQLHighlightMaterialTheme,
+            this.customTheme, EditorView.editable.of(false)
+          ],
           doc: this.expr,
         }),
         parent: doc
