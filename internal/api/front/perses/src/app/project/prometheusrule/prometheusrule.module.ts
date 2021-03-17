a6e0aaa882f228e164c72887aa9721e21e08c49e
Augustin Husson <husson.augustin@gmail.com>
2021-03-17 17:40:06 +0100

    improve a lot the display of the prometheusrule and use the page to focus only on the content of the page and not about how to manage the component in the page

--- internal/api/front/perses/src/app/project/prometheusrule/prometheusrule.module.ts
+++ internal/api/front/perses/src/app/project/prometheusrule/prometheusrule.module.ts
@@ -4,15 +4,17 @@ import { PrometheusRuleRoutingModule } from './prometheusrule-routing.module';
 import { SharedModule } from '../../shared/shared.module';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { PageModule } from '../../shared/component/page/page.module';
 
 @NgModule({
   declarations: [PrometheusRuleListComponent],
-  imports: [
-    MatDividerModule,
-    MatExpansionModule,
-    PrometheusRuleRoutingModule,
-    SharedModule,
-  ]
+    imports: [
+        MatDividerModule,
+        MatExpansionModule,
+        PrometheusRuleRoutingModule,
+        SharedModule,
+        PageModule,
+    ]
 })
 export class PrometheusRuleModule {
 }
