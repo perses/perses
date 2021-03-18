2507f05f80372e67331ac9a1698fed409b716197
Augustin Husson <husson.augustin@gmail.com>
2021-03-18 09:28:45 +0100

    fix interval field for ruleGroup and add some validation for the rule

--- pkg/model/api/v1/prometheusrule.go
+++ pkg/model/api/v1/prometheusrule.go
@@ -10,17 +10,51 @@ func GeneratePrometheusRuleID(project string, name string) string {
 }
 
 type Rule struct {
-	Record      string            `json:"record"`
-	Alert       string            `json:"alert"`
+	Record      string            `json:"record,omitempty"`
+	Alert       string            `json:"alert,omitempty"`
 	Expr        string            `json:"expr"`
 	For         string            `json:"for,omitempty"`
 	Labels      map[string]string `json:"labels,omitempty"`
 	Annotations map[string]string `json:"annotations,omitempty"`
 }
 
+func (r *Rule) UnmarshallJSON(data []byte) error {
+	var tmp Rule
+	type plain Rule
+	if err := json.Unmarshal(data, (*plain)(&tmp)); err != nil {
+		return err
+	}
+	if err := (&tmp).validate(); err != nil {
+		return err
+	}
+	*r = tmp
+	return nil
+}
+
+func (r *Rule) validate() error {
+	if len(r.Record) > 0 && len(r.Alert) > 0 {
+		return fmt.Errorf("only one of 'record' or 'alert' must be set")
+	}
+	if len(r.Record) == 0 && len(r.Alert) == 0 {
+		return fmt.Errorf("'record' or 'alert' must be set")
+	}
+	if len(r.Expr) == 0 {
+		return fmt.Errorf("field 'expr' must be set")
+	}
+	if len(r.Record) > 0 {
+		if len(r.Annotations) > 0 {
+			return fmt.Errorf("invalid field 'annotations' in recording rule")
+		}
+		if len(r.For) > 0 {
+			return fmt.Errorf("invalid field 'for' in recording rule")
+		}
+	}
+	return nil
+}
+
 type RuleGroup struct {
 	Name     string `json:"name"`
-	Internal string `json:"internal"`
+	Interval string `json:"interval,omitempty"`
 	Rules    []Rule `json:"rules"`
 }
 
