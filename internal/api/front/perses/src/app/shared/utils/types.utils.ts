20114eb82a9e9dd34c04a1bb3f37c8905995c8b1
Augustin Husson <husson.augustin@gmail.com>
2021-08-09 07:33:40 +0200

    remove angular app

--- internal/api/front/perses/src/app/shared/utils/types.utils.ts
+++
@@ -1,4 +0,0 @@
-import { Observable } from 'rxjs';
-import { UrlTree } from '@angular/router';
-
-export type CanActivateReturnType = Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree;
