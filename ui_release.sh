7f459e63ac41796822c79d2bbf86ef1162136cd2
Augustin Husson <husson.augustin@gmail.com>
2022-03-16 17:22:53 +0100

    split workflow in different jobs
    Signed-off-by: Augustin Husson <husson.augustin@gmail.com>

--- ui_release.sh
+++ ui_release.sh
@@ -17,10 +17,46 @@ function publish() {
   npm publish --workspaces
 }
 
+function checkPackage() {
+  version=${1}
+  if [[ "${version}" == v* ]]; then
+    version="${version:1}"
+  fi
+  for workspace in ${workspaces}; do
+    cd "${workspace}"
+    package_version=$(npm run env | grep npm_package_version | cut -d= -f2-)
+    if [ "${version}" != "${package_version}" ]; then
+      echo "version of ${workspace} is not the correct one"
+      echo "expected one: ${version}"
+      echo "current one: ${package_version}"
+      echo "please use ./ui_release --release ${version}"
+      exit 1
+    fi
+    cd ..
+  done
+}
+
+function prepareRelease() {
+  version=${1}
+  if [[ "${version}" == v* ]]; then
+    version="${version:1}"
+  fi
+  npm version "${version}" --workspaces
+}
+
 if [[ "$1" == "--copy" ]]; then
   copy
 fi
 
 if [[ $1 == "--publish" ]]; then
   publish
+fi
+
+
+if [[ $1 == "--check" ]]; then
+  checkPackage "${@:2}"
+fi
+
+if [[ $1 == "--release" ]]; then
+  prepareRelease "${@:2}"
 fi
