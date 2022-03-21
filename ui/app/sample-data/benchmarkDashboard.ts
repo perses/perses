c48912c5ff169a6b3a1b2fe930303933843b69d7
Steven Cobb <sjcobb.dev@gmail.com>
2022-03-21 09:11:34 -0400

    uncomment benchmarkDashboard
    Signed-off-by: Steven Cobb <sjcobb.dev@gmail.com>

--- ui/app/sample-data/benchmarkDashboard.ts
+++ ui/app/sample-data/benchmarkDashboard.ts
@@ -342,20 +342,20 @@ const benchmarkDashboard: DashboardResource = {
           },
         },
         items: [
-          // {
-          //   x: 0,
-          //   y: 0,
-          //   width: 12,
-          //   height: 6,
-          //   content: { $ref: '#/panels/seriesTestAlt' },
-          // },
-          // {
-          //   x: 12,
-          //   y: 0,
-          //   width: 12,
-          //   height: 6,
-          //   content: { $ref: '#/panels/multiQueries' },
-          // },
+          {
+            x: 0,
+            y: 0,
+            width: 12,
+            height: 6,
+            content: { $ref: '#/panels/seriesTestAlt' },
+          },
+          {
+            x: 12,
+            y: 0,
+            width: 12,
+            height: 6,
+            content: { $ref: '#/panels/multiQueries' },
+          },
         ],
       },
       {
