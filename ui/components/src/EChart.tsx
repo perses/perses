058775a51b177ad0464918328c90fa143cc8a47c
Steven Cobb <sjcobb.dev@gmail.com>
2022-04-25 17:02:29 -0400

    upd licen headers
    Signed-off-by: Steven Cobb <sjcobb.dev@gmail.com>

--- ui/components/src/EChart.tsx
+++ ui/components/src/EChart.tsx
@@ -1,10 +1,21 @@
+// Copyright 2022 The Perses Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import React, { useCallback, useEffect, useLayoutEffect, useRef } from 'react';
-// import React, { useCallback, useState, useLayoutEffect } from 'react';
 import debounce from 'lodash/debounce';
 import { ECharts, EChartsCoreOption, init } from 'echarts/core';
 import { Box, SxProps, Theme } from '@mui/material';
 import { isEqual } from 'lodash-es';
-// import { useDeepMemo } from '../utils';
 
 // https://echarts.apache.org/en/api.html#events
 export interface MouseEventsParameters<T> {
