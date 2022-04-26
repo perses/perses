40533e3d53488e9ecc8d99cbb652ec2f6f9a2a36
Steven Cobb <sjcobb.dev@gmail.com>
2022-04-26 15:13:20 -0400

    revert import useDeepMemo, add core as depen of components
    Signed-off-by: Steven Cobb <sjcobb.dev@gmail.com>

--- ui/components/src/LineChart.tsx
+++ ui/components/src/LineChart.tsx
@@ -12,6 +12,7 @@
 // limitations under the License.
 
 import React, { useMemo, useRef, useState } from 'react';
+import { useDeepMemo } from '@perses-dev/core';
 import { Box, useTheme } from '@mui/material';
 import merge from 'lodash/merge';
 import type {
@@ -37,7 +38,6 @@ import {
   VisualMapComponent,
 } from 'echarts/components';
 import { CanvasRenderer } from 'echarts/renderers';
-import { useDeepMemo } from './utils/memo';
 import { EChart, OnEventsType } from './EChart';
 import { PROGRESSIVE_MODE_SERIES_LIMIT, EChartsDataFormat } from './model/graph-model';
 import { abbreviateLargeNumber } from './model/units';
