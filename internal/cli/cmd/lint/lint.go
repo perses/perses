d20cfa2459abe233825ea10d93215c8eb4b1a702
Augustin Husson <husson.augustin@gmail.com>
2022-05-06 09:56:25 +0200

    CLI: lint cmd can load CUE schema to validate dashboards locally
    Signed-off-by: Augustin Husson <husson.augustin@gmail.com>

--- internal/cli/cmd/lint/lint.go
+++ internal/cli/cmd/lint/lint.go
@@ -17,21 +17,30 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/perses/perses/internal/api/impl/v1/dashboard/schemas"
 	cmdUtils "github.com/perses/perses/internal/cli/utils"
 	"github.com/perses/perses/internal/cli/utils/file"
+	"github.com/perses/perses/internal/config"
+	modelAPI "github.com/perses/perses/pkg/model/api"
+	modelV1 "github.com/perses/perses/pkg/model/api/v1"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
 type option struct {
 	cmdUtils.CMDOption
-	writer io.Writer
-	file   string
+	writer    io.Writer
+	file      string
+	schemas   string
+	validator schemas.Validator
 }
 
 func (o *option) Complete(args []string) error {
 	if len(args) > 0 {
 		return fmt.Errorf("no args are supported by the command 'lint'")
+	}
+	if len(o.schemas) > 0 {
+		o.validator = schemas.NewValidator(config.Schemas{Path: o.schemas})
 	}
 	return nil
 }
@@ -40,20 +49,43 @@ func (o *option) Validate() error {
 	if len(o.file) == 0 {
 		return fmt.Errorf("file must be provided")
 	}
+	if o.validator != nil {
+		if err := o.validator.Initialize(); err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
 func (o *option) Execute() error {
 	unmarshaller := file.Unmarshaller{}
-	_, err := unmarshaller.Unmarshal(o.file)
+	objects, err := unmarshaller.Unmarshal(o.file)
 	if err != nil {
 		return err
+	}
+	if validateErr := o.validate(objects); validateErr != nil {
+		return validateErr
 	}
 	return cmdUtils.HandleString(o.writer, "your resources look good")
 }
 
 func (o *option) SetWriter(writer io.Writer) {
 	o.writer = writer
+}
+
+func (o *option) validate(objects []modelAPI.Entity) error {
+	if o.validator == nil {
+		return nil
+	}
+	for _, object := range objects {
+		entity, ok := object.(*modelV1.Dashboard)
+		if ok {
+			if err := o.validator.Validate(entity.Spec.Panels); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
 }
 
 func NewCMD() *cobra.Command {
@@ -79,6 +111,7 @@ cat resources.json | percli lint -f -
 		},
 	}
 	cmd.Flags().StringVarP(&o.file, "file", "f", o.file, "Path to the file that contains the resources to check.")
+	cmd.Flags().StringVar(&o.schemas, "schemas", "", "Path to the CUE schemas.")
 	if err := cmd.MarkFlagRequired("file"); err != nil {
 		logrus.Fatal(err)
 	}
