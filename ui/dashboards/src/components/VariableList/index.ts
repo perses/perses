0906c51ea899fd35172a210b5658635e2ecedbb5
Luke Tillman <LukeTillman@users.noreply.github.com>
2022-08-08 10:03:51 -0600

    Add VariableList component
    Signed-off-by: Luke Tillman <LukeTillman@users.noreply.github.com>

--- ui/dashboards/src/components/VariableList/index.ts
+++ ui/dashboards/src/components/VariableList/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2021 The Perses Authors
+// Copyright 2022 The Perses Authors
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -11,9 +11,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-export * from './Dashboard';
-export * from './GridLayout';
-export * from './Panel/Panel';
-export * from './VariableAutocomplete';
 export * from './VariableList';
-export * from './VariableOptionsDrawer/VariableOptionsDrawer';
