87e2c8970a259a4a6299b7ef474fd28388dedc54
Steven Cobb <sjcobb.dev@gmail.com>
2022-09-07 15:00:24 -0400

    ViewDashboard lint fix
    Signed-off-by: Steven Cobb <sjcobb.dev@gmail.com>

--- ui/dashboards/src/views/ViewDashboard.tsx
+++ ui/dashboards/src/views/ViewDashboard.tsx
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Box, BoxProps, Stack, Typography } from '@mui/material';
+import { Box, BoxProps } from '@mui/material';
 import { combineSx } from '@perses-dev/components';
 import { DashboardResource } from '@perses-dev/core';
 import { TimeRangeStateProvider, TemplateVariablesProvider, DashboardProvider } from '../context';
