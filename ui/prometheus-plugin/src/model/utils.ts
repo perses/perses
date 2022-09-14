6ce90ae6f057823db16231f636c2413968c82af6
Shan Aminzadeh <shan.aminzadeh@chronosphere.io>
2022-09-14 10:54:51 -0400

    Add license
    Signed-off-by: Shan Aminzadeh <shan.aminzadeh@chronosphere.io>

--- ui/prometheus-plugin/src/model/utils.ts
+++ ui/prometheus-plugin/src/model/utils.ts
@@ -1,3 +1,16 @@
+// Copyright 2022 The Perses Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import { VariablesState, VariableValue } from '@perses-dev/core';
 
 export function replaceTemplateVariables(text: string, variableState: VariablesState): string {
@@ -14,7 +27,7 @@ export function replaceTemplateVariables(text: string, variableState: VariablesS
 }
 
 export function replaceTemplateVariable(text: string, varName: string, templateVariableValue: VariableValue) {
-  const variableTemplate = '\\$' + varName;
+  const variableTemplate = '\$' + varName;
   let replaceString = '';
   if (Array.isArray(templateVariableValue)) {
     replaceString = `(${templateVariableValue.join('|')})`; // regex style
