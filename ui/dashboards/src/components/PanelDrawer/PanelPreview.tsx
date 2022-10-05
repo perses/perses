2dd745bf5e6eb46a16747bd9540efc95f3dad296
Steven Cobb <sjcobb.dev@gmail.com>
2022-10-05 14:50:29 -0400

    fix build after PanelEditorValues import change
    Signed-off-by: Steven Cobb <sjcobb.dev@gmail.com>

--- ui/dashboards/src/components/PanelDrawer/PanelPreview.tsx
+++ ui/dashboards/src/components/PanelDrawer/PanelPreview.tsx
@@ -12,7 +12,7 @@
 // limitations under the License.
 
 import { Box } from '@mui/material';
-import { PanelEditorValues } from '../../context/DashboardProvider/panel-editing';
+import { PanelEditorValues } from '../../context/DashboardProvider/panel-editing-slice';
 import { Panel, PanelProps } from '../Panel';
 
 export function PanelPreview({ name, description, kind, spec, groupIndex }: PanelEditorValues) {
