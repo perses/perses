95973fa01b3c4a999edfb2c7c233133b4d5a4adb
Luke Tillman <LukeTillman@users.noreply.github.com>
2022-10-05 11:24:03 -0600

    Allow listing datasources for a given datasource plugin kind (#607)
    Signed-off-by: Luke Tillman <LukeTillman@users.noreply.github.com>
    Co-authored-by: Augustin Husson <husson.augustin@gmail.com>

--- ui/app/src/model/datasource-api.ts
+++ ui/app/src/model/datasource-api.ts
@@ -11,6 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import { Datasource, GlobalDatasource } from '@perses-dev/core';
 import { DatasourceStoreProviderProps } from '@perses-dev/dashboards';
 
 export function useDatasourceApi(): DatasourceStoreProviderProps['datasourceApi'] {
@@ -24,34 +25,57 @@ export function useDatasourceApi(): DatasourceStoreProviderProps['datasourceApi'
       // TODO: Convert selector to appropriate request params and fetchJson to get it from backend
 
       // Just resolve a default PrometheusDatasource right now
-      if (selector.kind === 'PrometheusDatasource' && selector.name === undefined) {
-        const name = 'PrometheusDemo';
+      if (selector.kind === tempDatasource.spec.plugin.kind && selector.name === undefined) {
         return {
-          resource: {
-            kind: 'GlobalDatasource',
-            metadata: {
-              name,
-              created_at: '',
-              updated_at: '',
-              version: 0,
-            },
-            spec: {
-              default: true,
-              display: {
-                name: 'Prometheus Demo',
-              },
-              plugin: {
-                kind: 'PrometheusDatasource',
-                spec: {
-                  direct_url: 'https://prometheus.demo.do.prometheus.io',
-                },
-              },
-            },
-          },
-          proxyUrl: `/proxy/globaldatasources/${encodeURIComponent(name)}`,
+          resource: tempDatasource,
+          proxyUrl: getProxyUrl(tempDatasource),
         };
       }
       return undefined;
     },
+
+    listDatasources: async (/*project, pluginKind*/) => {
+      return [];
+    },
+
+    listGlobalDatasources: async (pluginKind) => {
+      if (pluginKind === tempDatasource.spec.plugin.kind) {
+        return [tempDatasource];
+      }
+      return [];
+    },
   };
 }
+
+// Helper function for getting a proxy URL from a datasource or global datasource
+function getProxyUrl(datasource: Datasource | GlobalDatasource) {
+  let url = `/proxy`;
+  if (datasource.kind === 'Datasource') {
+    url += `/projects/${encodeURIComponent(datasource.metadata.project)}`;
+  }
+  url += `/${datasource.kind.toLowerCase()}s/${encodeURIComponent(datasource.metadata.name)}`;
+  return url;
+}
+
+// Just a temporary datasource while we're working on connecting to a real backend API
+const tempDatasource: GlobalDatasource = {
+  kind: 'GlobalDatasource',
+  metadata: {
+    name: 'PrometheusDemo',
+    created_at: '',
+    updated_at: '',
+    version: 0,
+  },
+  spec: {
+    default: true,
+    display: {
+      name: 'Prometheus Demo',
+    },
+    plugin: {
+      kind: 'PrometheusDatasource',
+      spec: {
+        direct_url: 'https://prometheus.demo.do.prometheus.io',
+      },
+    },
+  },
+};
--- ui/plugin-system/src/runtime/datasources.ts
+++ ui/plugin-system/src/runtime/datasources.ts
@@ -12,6 +12,7 @@
 // limitations under the License.
 
 import { DatasourceSelector, DatasourceSpec } from '@perses-dev/core';
+import { useQuery } from '@tanstack/react-query';
 import { createContext, useContext } from 'react';
 
 export interface DatasourceStore {
@@ -22,6 +23,16 @@ export interface DatasourceStore {
    * Given a DatasourceSelector, gets a `Client` object from the corresponding Datasource plugin.
    */
   getDatasourceClient<Client>(selector: DatasourceSelector): Promise<Client>;
+
+  /**
+   * Gets a list of datasource metadata for a plugin kind.
+   */
+  listDatasourceMetadata(datasourcePluginKind: string): Promise<DatasourceMetadata[]>;
+}
+
+export interface DatasourceMetadata {
+  name: string;
+  selector: DatasourceSelector;
 }
 
 export const DatasourceStoreContext = createContext<DatasourceStore | undefined>(undefined);
@@ -32,4 +43,13 @@ export function useDatasourceStore() {
     throw new Error('No DatasourceStoreContext found. Did you forget a Provider?');
   }
   return ctx;
+}
+
+/**
+ * Lists all available Datasource instances for a given datasource plugin kind. Returns a list with the name of that
+ * instance, as well as its DatasourceSelector for referencing it.
+ */
+export function useListDatasources(datasourcePluginKind: string) {
+  const { listDatasourceMetadata } = useDatasourceStore();
+  return useQuery(['listDatasourceMetadata', datasourcePluginKind], () => listDatasourceMetadata(datasourcePluginKind));
 }
