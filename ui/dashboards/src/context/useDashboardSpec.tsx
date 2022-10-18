534cc735a8f2664d8a9389c3bb94d1b78c883001
Eunice Wong <eunicewong@live.com>
2022-10-18 11:51:52 -0400

    fix license
    Signed-off-by: Eunice Wong <eunicewong@live.com>

--- ui/dashboards/src/context/useDashboardSpec.tsx
+++ ui/dashboards/src/context/useDashboardSpec.tsx
@@ -1,3 +1,16 @@
+// Copyright 2022 The Perses Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import { DashboardSpec, GridDefinition } from '@perses-dev/core';
 import { PanelGroupDefinition, useDashboardStore } from './DashboardProvider';
 import { useTemplateVariableDefinitions } from './TemplateVariableProvider';
@@ -11,7 +24,7 @@ export function useDashboardSpec(): DashboardSpec {
     panels,
     layouts,
     variables,
-    duration: '1h',
+    duration: '1h', // to do
   };
 }
 
