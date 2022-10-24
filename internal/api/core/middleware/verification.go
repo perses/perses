52eb8c9ad86fcbdcb940a9f28b229b35c1db64b6
Augustin Husson <husson.augustin@gmail.com>
2022-10-24 11:39:33 +0200

    optimize the condition to run the middleware
    Signed-off-by: Augustin Husson <husson.augustin@gmail.com>

--- internal/api/core/middleware/verification.go
+++ internal/api/core/middleware/verification.go
@@ -36,25 +36,28 @@ type partialObject struct {
 func CheckProject(dao project.DAO) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
+			// we don't need to verify if a project exists in case we are in a PUT / GET / DELETE request since if the project doesn't exist, then the dashboard won't exist either.
+			// Also, we avoid an additional query to the DB like that.
+			if c.Request().Method != http.MethodPost {
+				return next(c)
+			}
 			projectName := shared.GetProjectParameter(c)
 			if len(projectName) == 0 {
 				// It's possible the HTTP Path doesn't contain the project because the user is calling the root endpoint
 				// to create a new dashboard for example.
 				// So we need to ensure the project name exists in the resource, which is why we will partially decode the body to get the project name.
 				// And just to avoid a non-necessary deserialization, we will ensure we are managing a resource that is part of a project by checking the HTTP Path.
-				if c.Request().Method == http.MethodPost {
-					for _, path := range shared.ProjectResourcePathList {
-						if strings.HasPrefix(c.Path(), fmt.Sprintf("%s/%s", shared.APIV1Prefix, path)) {
-							o := &partialObject{}
-							if err := c.Bind(o); err != nil {
-								return err
-							}
-							if len(o.Metadata.Project) == 0 {
-								return shared.HandleError(fmt.Errorf("%w: metadata.project cannot be empty", shared.BadRequestError))
-							}
-							projectName = o.Metadata.Project
-							break
+				for _, path := range shared.ProjectResourcePathList {
+					if strings.HasPrefix(c.Path(), fmt.Sprintf("%s/%s", shared.APIV1Prefix, path)) {
+						o := &partialObject{}
+						if err := c.Bind(o); err != nil {
+							return err
 						}
+						if len(o.Metadata.Project) == 0 {
+							return shared.HandleError(fmt.Errorf("%w: metadata.project cannot be empty", shared.BadRequestError))
+						}
+						projectName = o.Metadata.Project
+						break
 					}
 				}
 			}
