f87a7069c93bf8aeff6d6725eded3de475992f80
Shan Aminzadeh <shan.aminzadeh@chronosphere.io>
2022-10-26 11:46:56 -0400

    Implement variable state in URL parameters (#690)
    * Implement variable state in URL parameters
    
    Signed-off-by: Shan Aminzadeh <shan.aminzadeh@chronosphere.io>
    
    * Remove fragment
    
    Signed-off-by: Shan Aminzadeh <shan.aminzadeh@chronosphere.io>
    
    * Update license
    
    Signed-off-by: Shan Aminzadeh <shan.aminzadeh@chronosphere.io>
    
    * Code review comments and unit tests
    
    Signed-off-by: Shan Aminzadeh <shan.aminzadeh@chronosphere.io>
    
    Signed-off-by: Shan Aminzadeh <shan.aminzadeh@chronosphere.io>

--- ui/dashboards/src/components/Variables/Variable.tsx
+++ ui/dashboards/src/components/Variables/Variable.tsx
@@ -111,25 +111,26 @@ function ListVariable({ name }: TemplateVariableProps) {
     return computedOptions;
   }, [options, allowAllValue]);
 
+  const valueIsInOptions = useMemo(
+    () =>
+      Boolean(
+        finalOptions.find((v) => {
+          if (allowMultiple) {
+            return (value as string[]).includes(v.value);
+          }
+          return value === v.value;
+        })
+      ),
+    [finalOptions, value, allowMultiple]
+  );
+  const selectValue = valueIsInOptions ? value ?? '' : '';
+
   useEffect(() => {
     const firstOption = finalOptions?.[0];
-    const valueIsInOptions = Boolean(
-      finalOptions.find((v) => {
-        if (allowMultiple) {
-          return (value as string[]).includes(v.value);
-        }
-        return value === v.value;
-      })
-    );
 
     // If there is no value but there are options, set the value to the first option.
     if (!value && firstOption) {
       setVariableValue(name, firstOption.value);
-    }
-
-    // If there is a value but it's not in the options, select the first value or set to null
-    if (value && !valueIsInOptions && !definition.spec.default_value) {
-      setVariableValue(name, firstOption?.value || null);
     }
   }, [finalOptions, setVariableValue, value, name, allowMultiple]);
 
@@ -141,7 +142,7 @@ function ListVariable({ name }: TemplateVariableProps) {
           sx={{ minWidth: 100, maxWidth: 250 }}
           id={name}
           label={name}
-          value={value ?? ''}
+          value={selectValue}
           onChange={(e) => {
             // Must be selected
             if (e.target.value === null || e.target.value.length === 0) {
