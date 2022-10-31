3f10d8b9ef95398b752294e438b03c663cfb3378
Eunice Wong <eunicewong@live.com>
2022-10-31 14:29:01 -0400

    remove eslint
    Signed-off-by: Eunice Wong <eunicewong@live.com>

--- ui/dashboards/src/components/UnsavedChangesConfirmationDialog/UnsavedChangesConfirmationDialog.tsx
+++ ui/dashboards/src/components/UnsavedChangesConfirmationDialog/UnsavedChangesConfirmationDialog.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-empty-function */
-
 // Copyright 2022 The Perses Authors
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
