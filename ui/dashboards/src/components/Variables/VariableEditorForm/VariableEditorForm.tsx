330bd9666b9ce269631a74b81e89b2b7ea211106
Christine Donovan <christine.donovan@chronosphere.io>
2022-11-17 13:15:00 -0800

    Make the headers in editors all look the same (i.e. panel + variable editing)
    Signed-off-by: Christine Donovan <christine.donovan@chronosphere.io>

--- ui/dashboards/src/components/Variables/VariableEditorForm/VariableEditorForm.tsx
+++ ui/dashboards/src/components/Variables/VariableEditorForm/VariableEditorForm.tsx
@@ -123,95 +123,58 @@ export function VariableEditForm({
   }, [previewKey]);
 
   return (
-    <Box>
-      <SectionHeader>General</SectionHeader>
-      <Grid container spacing={2} mb={2}>
-        <Grid item xs={6}>
-          <TextField
-            required
-            error={!!validation.name}
-            fullWidth
-            label="Name"
-            value={state.name}
-            helperText={validation.name}
-            onChange={(v) => {
-              setState((draft) => {
-                draft.name = v.target.value as string;
-              });
+    <>
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          padding: (theme) => theme.spacing(1, 2),
+          borderBottom: (theme) => `1px solid ${theme.palette.grey[100]}`,
+        }}
+      >
+        <Typography variant="h2">Template Variables / Edit Variable</Typography>
+        <Stack direction="row" spacing={1} sx={{ marginLeft: 'auto' }}>
+          <Button
+            disabled={!validation.isValid}
+            variant="contained"
+            onClick={() => {
+              onChange(getVariableDefinitionFromState(state));
             }}
-          />
-        </Grid>
-        <Grid item xs={6}>
-          <FormControl fullWidth>
-            <InputLabel id="variable-type-select-label">Type</InputLabel>
-            <Select
-              labelId="variable-type-select-label"
-              id="variable-type-select"
-              label="Type"
-              value={state.kind}
-              onChange={(v) => {
-                setState((draft) => {
-                  draft.kind = v.target.value as VariableEditorState['kind'];
-                });
-              }}
-            >
-              {VARIABLE_TYPES.map((v) => (
-                <MenuItem key={v} value={v}>
-                  {v}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-        </Grid>
-        <Grid item xs={6}>
-          <TextField
-            fullWidth
-            label="Label"
-            value={state.title}
-            onChange={(v) => {
-              setState((draft) => {
-                draft.title = v.target.value;
-              });
+          >
+            Update
+          </Button>
+          <Button
+            variant="outlined"
+            onClick={() => {
+              onCancel();
             }}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            fullWidth
-            label="Description"
-            value={state.description}
-            onChange={(v) => {
-              setState((draft) => {
-                draft.description = v.target.value;
-              });
-            }}
-          />
-        </Grid>
-      </Grid>
-
-      {state.kind === 'TextVariable' && (
-        <>
-          <SectionHeader>Text Options</SectionHeader>
+          >
+            Cancel
+          </Button>
+        </Stack>
+      </Box>
+      <Box p={2}>
+        <Typography variant="h3" mb={2}>
+          Edit Variable
+        </Typography>
+        <Box>
+          <SectionHeader>General</SectionHeader>
           <Grid container spacing={2} mb={2}>
-            <Grid item xs={12}>
+            <Grid item xs={6}>
               <TextField
-                label="Value"
-                value={state.textVariableFields.value}
+                required
+                error={!!validation.name}
+                fullWidth
+                label="Name"
+                value={state.name}
+                helperText={validation.name}
                 onChange={(v) => {
                   setState((draft) => {
-                    draft.textVariableFields.value = v.target.value;
+                    draft.name = v.target.value as string;
                   });
                 }}
               />
             </Grid>
-          </Grid>
-        </>
-      )}
-
-      {state.kind === 'ListVariable' && (
-        <>
-          <SectionHeader>List Options</SectionHeader>
-          <Grid container spacing={2} mb={2}>
             <Grid item xs={6}>
               {/** Hack?: Cool technique to refresh the preview to simulate onBlur event */}
               <ClickAwayListener onClickAway={() => refreshPreview()}>
@@ -251,45 +214,92 @@ export function VariableEditForm({
                 checked={state.listVariableFields.allowMultiple}
                 onChange={(e) => {
                   setState((draft) => {
-                    draft.listVariableFields.allowMultiple = e.target.checked;
+                    draft.title = v.target.value;
                   });
                 }}
               />
             </Grid>
             <Grid item xs={12}>
-              Allow All
-              <Switch
-                checked={state.listVariableFields.allowAll}
-                onChange={(e) => {
+              <TextField
+                fullWidth
+                label="Description"
+                value={state.description}
+                onChange={(v) => {
                   setState((draft) => {
-                    draft.listVariableFields.allowAll = e.target.checked;
+                    draft.description = v.target.value;
                   });
                 }}
               />
             </Grid>
           </Grid>
-        </>
-      )}
 
-      <Stack direction={'row'} spacing={2} justifyContent="end">
-        <Button
-          disabled={!validation.isValid}
-          variant="contained"
-          onClick={() => {
-            onChange(getVariableDefinitionFromState(state));
-          }}
-        >
-          Update
-        </Button>
-        <Button
-          variant="outlined"
-          onClick={() => {
-            onCancel();
-          }}
-        >
-          Cancel
-        </Button>
-      </Stack>
-    </Box>
+          {state.kind === 'TextVariable' && (
+            <>
+              <SectionHeader>Text Options</SectionHeader>
+              <Grid container spacing={2} mb={2}>
+                <Grid item xs={12}>
+                  <TextField
+                    label="Value"
+                    value={state.textVariableFields.value}
+                    onChange={(v) => {
+                      setState((draft) => {
+                        draft.textVariableFields.value = v.target.value;
+                      });
+                    }}
+                  />
+                </Grid>
+              </Grid>
+            </>
+          )}
+
+          {state.kind === 'ListVariable' && (
+            <>
+              <SectionHeader>List Options</SectionHeader>
+              <Grid container spacing={2} mb={2}>
+                <Grid item xs={6}>
+                  <PluginEditor
+                    width={500}
+                    pluginType="Variable"
+                    pluginKindLabel="Source"
+                    value={state.listVariableFields.plugin}
+                    onChange={(val) => {
+                      setState((draft) => {
+                        draft.listVariableFields.plugin = val;
+                      });
+                    }}
+                  />
+                </Grid>
+              </Grid>
+
+              <SectionHeader>Dropdown Options</SectionHeader>
+              <Grid container spacing={1} mb={1}>
+                <Grid item xs={12}>
+                  Allow Multiple
+                  <Switch
+                    checked={state.listVariableFields.allowMultiple}
+                    onChange={(e) => {
+                      setState((draft) => {
+                        draft.listVariableFields.allowMultiple = e.target.checked;
+                      });
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  Allow All
+                  <Switch
+                    checked={state.listVariableFields.allowAll}
+                    onChange={(e) => {
+                      setState((draft) => {
+                        draft.listVariableFields.allowAll = e.target.checked;
+                      });
+                    }}
+                  />
+                </Grid>
+              </Grid>
+            </>
+          )}
+        </Box>
+      </Box>
+    </>
   );
 }
