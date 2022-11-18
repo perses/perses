f2295206dfd94a595edd152f126e2a0cb1fe1127
Augustin Husson <husson.augustin@gmail.com>
2022-11-18 16:39:35 +0100

    fix creation of entity with a name containing space
    Signed-off-by: Augustin Husson <husson.augustin@gmail.com>

--- internal/api/e2e/dashboard_test.go
+++ internal/api/e2e/dashboard_test.go
@@ -16,15 +16,33 @@
 package e2e
 
 import (
+	"fmt"
+	"net/http"
 	"testing"
 
+	"github.com/gavv/httpexpect/v2"
 	e2eframework "github.com/perses/perses/internal/api/e2e/framework"
 	"github.com/perses/perses/internal/api/shared"
+	"github.com/perses/perses/internal/api/shared/dependency"
 	"github.com/perses/perses/pkg/model/api"
 )
 
 func TestMainScenarioDashboard(t *testing.T) {
 	e2eframework.MainTestScenarioWithProject(t, shared.PathDashboard, func(projectName string, name string) (api.Entity, api.Entity) {
 		return e2eframework.NewProject(projectName), e2eframework.NewDashboard(t, projectName, name)
+	})
+}
+
+func TestCreateDashboardWithWrongName(t *testing.T) {
+	e2eframework.WithServer(t, func(expect *httpexpect.Expect, manager dependency.PersistenceManager) []api.Entity {
+		entity := e2eframework.NewDashboard(t, "perses", "Incorrect Name With Space")
+		project := e2eframework.NewProject("perses")
+		e2eframework.CreateAndWaitUntilEntityExists(t, manager, project)
+
+		expect.POST(fmt.Sprintf("%s/%s/%s/%s", shared.APIV1Prefix, shared.PathProject, "perses", shared.PathDashboard)).
+			WithJSON(entity).
+			Expect().
+			Status(http.StatusBadRequest)
+		return []api.Entity{project}
 	})
 }
