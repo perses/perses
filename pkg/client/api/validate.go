75d85edb0bcc27fc65c0f1f94f8ffe9320edb17b
Augustin Husson <husson.augustin@gmail.com>
2022-12-09 18:01:42 +0100

    [FEATURE] Add new endpoints to validate dashboard and datasource
    Signed-off-by: Augustin Husson <husson.augustin@gmail.com>

---
+++ pkg/client/api/validate.go
@@ -0,0 +1,59 @@
+// Copyright 2022 The Perses Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package api
+
+import (
+	"github.com/perses/perses/pkg/client/perseshttp"
+	v1 "github.com/perses/perses/pkg/model/api/v1"
+)
+
+type ValidateInterface interface {
+	Dashboard(entity *v1.Dashboard) error
+	Datasource(entity *v1.Datasource) error
+	GlobalDatasource(entity *v1.GlobalDatasource) error
+}
+
+type validate struct {
+	ValidateInterface
+	client *perseshttp.RESTClient
+}
+
+func newValidate(client *perseshttp.RESTClient) ValidateInterface {
+	return &validate{client: client}
+}
+
+func (c *validate) Dashboard(entity *v1.Dashboard) error {
+	return c.client.Post().
+		APIVersion("").
+		Resource("validate/dashboards").
+		Body(entity).
+		Do().
+		Error()
+}
+func (c *validate) Datasource(entity *v1.Datasource) error {
+	return c.client.Post().
+		APIVersion("").
+		Resource("validate/datasources").
+		Body(entity).
+		Do().
+		Error()
+}
+func (c *validate) GlobalDatasource(entity *v1.GlobalDatasource) error {
+	return c.client.Post().
+		APIVersion("").
+		Resource("validate/globaldatasources").
+		Body(entity).
+		Do().
+		Error()
+}
