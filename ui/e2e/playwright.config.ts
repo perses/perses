6fa353bc9444fccdfdd21fa39e6f562fe227e051
Julie Pagano <julie.pagano@chronosphere.io>
2023-01-04 11:54:15 -0800

    Increase test timeout to a minute
    Signed-off-by: Julie Pagano <julie.pagano@chronosphere.io>

--- ui/e2e/playwright.config.ts
+++ ui/e2e/playwright.config.ts
@@ -23,7 +23,7 @@ const IS_CI = process.env.CI;
 const config: PlaywrightTestConfig = {
   testDir: './src/tests',
   /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
+  timeout: 60 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
