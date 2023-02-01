a8939870182c8555e5018c79982f430b91138c7d
Julie Pagano <julie.pagano@chronosphere.io>
2023-02-01 13:49:13 -0800

    Clarify time series comment
    Signed-off-by: Julie Pagano <julie.pagano@chronosphere.io>

--- ui/panels-plugin/src/plugins/time-series-chart/TimeSeriesChartPanel.tsx
+++ ui/panels-plugin/src/plugins/time-series-chart/TimeSeriesChartPanel.tsx
@@ -52,6 +52,8 @@ export function TimeSeriesChartPanel(props: TimeSeriesChartProps) {
   // TODO: consider refactoring how the layout/spacing/alignment are calculated
   // the next time significant changes are made to the time series panel (e.g.
   // when making improvements to the legend to more closely match designs).
+  // This may also want to include moving some of this logic down to the shared,
+  // embeddable components.
   const contentPadding = chartsTheme.container.padding.default;
   const adjustedContentDimensions: typeof contentDimensions = contentDimensions
     ? {
