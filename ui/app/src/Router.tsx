b484cba438309315d6d31562295f3e1d5a354c57
Steven Cobb <sjcobb.dev@gmail.com>
2023-02-06 10:33:38 -0500

    [ENHANCEMENT] project routing and list view links
    Signed-off-by: Steven Cobb <sjcobb.dev@gmail.com>

--- ui/app/src/Router.tsx
+++ ui/app/src/Router.tsx
@@ -29,6 +29,7 @@ function Router() {
       <Suspense>
         <Routes>
           <Route path="/migrate" element={<ViewMigrate />} />
+          <Route path="/projects" element={<ViewDashboardList />} />
           <Route path="/projects/:projectName/dashboards/:dashboardName" element={<ViewDashboard />} />
           <Route path="/projects/:projectName" element={<ViewProject />} />
           <Route path="/" element={<ViewDashboardList />} />
