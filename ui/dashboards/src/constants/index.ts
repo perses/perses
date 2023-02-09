653c33d23b87cea1c8c74e416652a75e03568cdd
Julie Pagano <julie.pagano@chronosphere.io>
2023-02-09 11:57:41 -0800

    alphabetize imports
    Signed-off-by: Julie Pagano <julie.pagano@chronosphere.io>

--- ui/dashboards/src/constants/index.ts
+++ ui/dashboards/src/constants/index.ts
@@ -11,5 +11,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-export * from './user-interface-text';
 export * from './grid-layout-config';
+export * from './user-interface-text';
