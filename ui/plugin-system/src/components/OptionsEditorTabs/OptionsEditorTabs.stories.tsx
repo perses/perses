da7bfef9a330b1516cc9bee99b4817c964540300
Julie Pagano <julie.pagano@chronosphere.io>
2023-02-22 12:14:20 -0800

    Moar relative
    Signed-off-by: Julie Pagano <julie.pagano@chronosphere.io>

--- ui/plugin-system/src/components/OptionsEditorTabs/OptionsEditorTabs.stories.tsx
+++ ui/plugin-system/src/components/OptionsEditorTabs/OptionsEditorTabs.stories.tsx
@@ -14,7 +14,7 @@
 import React from 'react';
 
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { OptionsEditorTabs } from '@perses-dev/plugin-system';
+import { OptionsEditorTabs } from '../../../src';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
