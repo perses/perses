45ee1edfbfdc6c9889d84a3b760a8c81c1398244
Steven Cobb <sjcobb.dev@gmail.com>
2023-02-24 11:12:54 -0500

    add visual options happo test for time series panel
    Signed-off-by: Steven Cobb <sjcobb.dev@gmail.com>

--- ui/e2e/src/tests/timeSeriesChartPanel.spec.ts
+++ ui/e2e/src/tests/timeSeriesChartPanel.spec.ts
@@ -69,4 +69,56 @@ test.describe('Dashboard: Time Series Chart Panel', () => {
       });
     });
   });
+
+  test(`displays time series with custom visual options`, async ({ page, dashboardPage, mockNow }) => {
+    // Mock data response, so we can make assertions on consistent response data.
+    await dashboardPage.mockQueryRangeRequests({
+      queries: [
+        {
+          query: 'up{job="grafana",instance="demo.do.prometheus.io:3000"}',
+          response: {
+            status: 200,
+            body: JSON.stringify(
+              mockTimeSeriesResponseWithStableValue({
+                metrics: [
+                  {
+                    metric: {
+                      __name__: 'up',
+                      instance: 'demo.do.prometheus.io:3000',
+                      job: 'grafana',
+                    },
+                    value: '1',
+                  },
+                ],
+                startTimeMs: mockNow - 6 * 60 * 60 * 1000,
+                endTimeMs: mockNow,
+              })
+            ),
+          },
+        },
+      ],
+    });
+
+    await dashboardPage.forEachTheme(async (themeName) => {
+      const panel = dashboardPage.getPanelByName('Single Line');
+      await panel.isLoaded();
+      await waitForStableCanvas(panel.canvas);
+
+      await happoPlaywright.screenshot(page, panel.parent, {
+        component: 'Time Series Chart Panel',
+        variant: `Single Line [${themeName}]`,
+      });
+    });
+
+    await dashboardPage.forEachTheme(async (themeName) => {
+      const panel = dashboardPage.getPanelByName('Line Visual Options');
+      await panel.isLoaded();
+      await waitForStableCanvas(panel.canvas);
+
+      await happoPlaywright.screenshot(page, panel.parent, {
+        component: 'Time Series Chart Panel',
+        variant: `Line Visual Options [${themeName}]`,
+      });
+    });
+  });
 });
