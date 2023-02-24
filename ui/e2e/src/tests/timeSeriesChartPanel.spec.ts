265743653c350ad2c82b68093c89f30b9f45e15f
Steven Cobb <sjcobb.dev@gmail.com>
2023-02-24 12:30:29 -0500

    combine single line tests to fix errors
    Signed-off-by: Steven Cobb <sjcobb.dev@gmail.com>

--- ui/e2e/src/tests/timeSeriesChartPanel.spec.ts
+++ ui/e2e/src/tests/timeSeriesChartPanel.spec.ts
@@ -29,7 +29,11 @@ test.describe('Dashboard: Time Series Chart Panel', () => {
     await happoPlaywright.finish();
   });
 
-  test(`displays single line as expected`, async ({ page, dashboardPage, mockNow }) => {
+  test(`displays default single line and custom visual options as expected`, async ({
+    page,
+    dashboardPage,
+    mockNow,
+  }) => {
     // Mock data response, so we can make assertions on consistent response data.
     await dashboardPage.mockQueryRangeRequests({
       queries: [
@@ -59,63 +63,20 @@ test.describe('Dashboard: Time Series Chart Panel', () => {
     });
 
     await dashboardPage.forEachTheme(async (themeName) => {
-      const panel = dashboardPage.getPanelByName('Single Line');
-      await panel.isLoaded();
-      await waitForStableCanvas(panel.canvas);
+      const panelFirst = dashboardPage.getPanelByName('Single Line');
+      await panelFirst.isLoaded();
+      await waitForStableCanvas(panelFirst.canvas);
 
-      await happoPlaywright.screenshot(page, panel.parent, {
+      await happoPlaywright.screenshot(page, panelFirst.parent, {
         component: 'Time Series Chart Panel',
         variant: `Single Line [${themeName}]`,
       });
-    });
-  });
 
-  test(`displays time series with custom visual options`, async ({ page, dashboardPage, mockNow }) => {
-    // Mock data response, so we can make assertions on consistent response data.
-    await dashboardPage.mockQueryRangeRequests({
-      queries: [
-        {
-          query: 'up{job="grafana",instance="demo.do.prometheus.io:3000"}',
-          response: {
-            status: 200,
-            body: JSON.stringify(
-              mockTimeSeriesResponseWithStableValue({
-                metrics: [
-                  {
-                    metric: {
-                      __name__: 'up',
-                      instance: 'demo.do.prometheus.io:3000',
-                      job: 'grafana',
-                    },
-                    value: '1',
-                  },
-                ],
-                startTimeMs: mockNow - 6 * 60 * 60 * 1000,
-                endTimeMs: mockNow,
-              })
-            ),
-          },
-        },
-      ],
-    });
+      const panelSecond = dashboardPage.getPanelByName('Line Visual Options');
+      await panelSecond.isLoaded();
+      await waitForStableCanvas(panelSecond.canvas);
 
-    await dashboardPage.forEachTheme(async (themeName) => {
-      const panel = dashboardPage.getPanelByName('Single Line');
-      await panel.isLoaded();
-      await waitForStableCanvas(panel.canvas);
-
-      await happoPlaywright.screenshot(page, panel.parent, {
-        component: 'Time Series Chart Panel',
-        variant: `Single Line [${themeName}]`,
-      });
-    });
-
-    await dashboardPage.forEachTheme(async (themeName) => {
-      const panel = dashboardPage.getPanelByName('Line Visual Options');
-      await panel.isLoaded();
-      await waitForStableCanvas(panel.canvas);
-
-      await happoPlaywright.screenshot(page, panel.parent, {
+      await happoPlaywright.screenshot(page, panelSecond.parent, {
         component: 'Time Series Chart Panel',
         variant: `Line Visual Options [${themeName}]`,
       });
