d49c40a531ce212ef5596b2f4e5b53f591033eef
Julie Pagano <julie.pagano@chronosphere.io>
2023-02-25 14:53:15 -0800

    Add group button
    Signed-off-by: Julie Pagano <julie.pagano@chronosphere.io>

---
+++ ui/dashboards/src/components/AddGroupButton/AddGroupButton.stories.tsx
@@ -0,0 +1,66 @@
+// Copyright 2023 The Perses Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import type { Meta, StoryObj } from '@storybook/react';
+import { AddGroupButton, DashboardProvider } from '@perses-dev/dashboards';
+import { PluginRegistry } from '@perses-dev/plugin-system';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const meta: Meta<typeof AddGroupButton> = {
+  component: AddGroupButton,
+  render: () => {
+    const queryClient = new QueryClient({});
+
+    return (
+      <QueryClientProvider client={queryClient}>
+        <PluginRegistry
+          pluginLoader={{
+            getInstalledPlugins: () => Promise.resolve([]),
+            importPluginModule: () => Promise.resolve(),
+          }}
+        >
+          <DashboardProvider
+            initialState={{
+              dashboardResource: {
+                kind: 'Dashboard',
+                metadata: {
+                  name: 'AddGroupButton',
+                  project: 'storybook',
+                  created_at: '2021-11-09T00:00:00Z',
+                  updated_at: '2021-11-09T00:00:00Z',
+                  version: 0,
+                },
+                spec: {
+                  duration: '6h',
+                  variables: [],
+                  layouts: [],
+                  panels: {},
+                },
+              },
+            }}
+          >
+            <AddGroupButton />
+          </DashboardProvider>
+        </PluginRegistry>
+      </QueryClientProvider>
+    );
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof AddGroupButton>;
+
+export const Primary: Story = {
+  args: {},
+};
