1824a012240821f1459ddefb3a10ec68cb882e3a
Julie Pagano <julie.pagano@chronosphere.io>
2023-03-07 13:37:36 -0800

    [IGNORE] Update happo and remove v7 customizations (#1006)
    The helpful folks over at Happo took our feedback and implemented
    support for both storybook v7 and the theme switching, so we can remove
    a bunch of the customizations.
    
    Signed-off-by: Julie Pagano <julie.pagano@chronosphere.io>

--- ui/storybook/src/types/happo.d.ts
+++ ui/storybook/src/types/happo.d.ts
@@ -11,8 +11,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// This file contains barebones types needed to make TS code play nicely with
-// register.js, which is being kept in JS in the hopes that it can eventually
-// be used directly from Happo in the future after some improvements.
+/// <reference types="@storybook/addons" />
 
-export function isHappoRun(): boolean;
+// Minimum implementation to get happo working with TS.
+// https://github.com/happo/happo-plugin-storybook
+
+declare module 'happo-plugin-storybook/register' {
+  export function isHappoRun(): boolean;
+  export function setThemeSwitcher(themeSwitcherFn: (theme: string, channel: Channel) => Promise<void>);
+}
