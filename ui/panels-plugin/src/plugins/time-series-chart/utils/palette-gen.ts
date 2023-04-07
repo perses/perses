ab4a9d3996f718c3d031c03d6a4ae288708c9390
Steven Cobb <sjcobb.dev@gmail.com>
2023-04-07 11:08:10 -0400

    try stackoverflow approach using veryDifferentColors array to gen hex values from string
    Signed-off-by: Steven Cobb <sjcobb.dev@gmail.com>

--- ui/panels-plugin/src/plugins/time-series-chart/utils/palette-gen.ts
+++ ui/panels-plugin/src/plugins/time-series-chart/utils/palette-gen.ts
@@ -45,5 +45,95 @@ export function getSeriesColor(
   }
 
   // corresponds to 'Auto' in palette.kind
-  return getRandomColor(name);
+  // return getRandomColor(name);
+  const generatedColor = stringToColor.next(name);
+  return generatedColor;
 }
+
+/*
+ * Color conversion from string using palette for contrast
+ * https://stackoverflow.com/a/31037383/17575201
+ */
+
+const stringToColor = (function () {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  let instance: any = null;
+  return {
+    next: function stringToColor(str: string) {
+      if (instance === null) {
+        instance = {};
+        instance.stringToColorHash = {};
+        instance.nextVeryDifferntColorIdx = 0;
+        instance.veryDifferentColors = [
+          '#000000',
+          '#00FF00',
+          '#0000FF',
+          '#FF0000',
+          '#01FFFE',
+          '#FFA6FE',
+          '#FFDB66',
+          '#006401',
+          '#010067',
+          '#95003A',
+          '#007DB5',
+          '#FF00F6',
+          '#FFEEE8',
+          '#774D00',
+          '#90FB92',
+          '#0076FF',
+          '#D5FF00',
+          '#FF937E',
+          '#6A826C',
+          '#FF029D',
+          '#FE8900',
+          '#7A4782',
+          '#7E2DD2',
+          '#85A900',
+          '#FF0056',
+          '#A42400',
+          '#00AE7E',
+          '#683D3B',
+          '#BDC6FF',
+          '#263400',
+          '#BDD393',
+          '#00B917',
+          '#9E008E',
+          '#001544',
+          '#C28C9F',
+          '#FF74A3',
+          '#01D0FF',
+          '#004754',
+          '#E56FFE',
+          '#788231',
+          '#0E4CA1',
+          '#91D0CB',
+          '#BE9970',
+          '#968AE8',
+          '#BB8800',
+          '#43002C',
+          '#DEFF74',
+          '#00FFC6',
+          '#FFE502',
+          '#620E00',
+          '#008F9C',
+          '#98FF52',
+          '#7544B1',
+          '#B500FF',
+          '#00FF78',
+          '#FF6E41',
+          '#005F39',
+          '#6B6882',
+          '#5FAD4E',
+          '#A75740',
+          '#A5FFD2',
+          '#FFB167',
+          '#009BFF',
+          '#E85EBE',
+        ];
+      }
+      if (!instance.stringToColorHash[str])
+        instance.stringToColorHash[str] = instance.veryDifferentColors[instance.nextVeryDifferntColorIdx++];
+      return instance.stringToColorHash[str] as string;
+    },
+  };
+})();
