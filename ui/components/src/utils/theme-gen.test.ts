f2383087514b7981eb0cc4318020357d164be8cf
Steven Cobb <sjcobb.dev@gmail.com>
2023-04-18 12:20:53 -0400

    fix palette-gen.test.ts and theme-gen.test.ts with new colors
    Signed-off-by: Steven Cobb <sjcobb.dev@gmail.com>

--- ui/components/src/utils/theme-gen.test.ts
+++ ui/components/src/utils/theme-gen.test.ts
@@ -84,11 +84,18 @@ describe('generateChartsTheme', () => {
             },
           },
           "color": Array [
-            "#8dd3c7",
-            "#fb8072",
-            "#80b1d3",
-            "#bebada",
-            "#fdb462",
+            "#a6cee3",
+            "#1f78b4",
+            "#b2df8a",
+            "#33a02c",
+            "#fb9a99",
+            "#e31a1c",
+            "#fdbf6f",
+            "#ff7f00",
+            "#cab2d6",
+            "#6a3d9a",
+            "#ffff99",
+            "#b15928",
           ],
           "gauge": Object {
             "detail": Object {
--- ui/panels-plugin/src/plugins/time-series-chart/utils/palette-gen.test.ts
+++ ui/panels-plugin/src/plugins/time-series-chart/utils/palette-gen.test.ts
@@ -36,7 +36,7 @@ describe('getAutoPaletteColor', () => {
   const fallbackColor = '#ff0000';
   it('should auto generate a color from the series name', () => {
     const generatedColor = getAutoPaletteColor('Incoming Writes per second', fallbackColor);
-    expect(generatedColor).toEqual('hsla(243.48,65%,65%,0.9)');
+    expect(generatedColor).toEqual('hsla(250.15,65%,65%,0.9)');
   });
 
   it('should generate a unique color from the series name', () => {
@@ -44,7 +44,7 @@ describe('getAutoPaletteColor', () => {
       'node_memory_Buffers_bytes{env="demo",instance="demo.do.prometheus.io:9100",job="node"}',
       fallbackColor
     );
-    expect(generatedColor).toEqual('hsla(132.56,50%,35%,0.9)');
+    expect(generatedColor).toEqual('hsla(135.76,50%,35%,0.9)');
   });
 });
 
@@ -52,7 +52,7 @@ describe('getConsistentSeriesNameColor', () => {
   it('should generate a consistent custom hsla color', () => {
     const color = getConsistentSeriesNameColor('test');
     const colorAlt = getConsistentSeriesNameColor('test');
-    const firstResult = 'hsla(283.54,35%,50%,0.9)';
+    const firstResult = 'hsla(291.46,35%,50%,0.9)';
     // ensures generated color does not change on subsequent calls with same series name
     expect(color).toEqual(firstResult);
     expect(colorAlt).toEqual(firstResult);
