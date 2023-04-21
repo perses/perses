83328843dd6defef1224fe87acb5f50196b725bc
Christine Donovan <2584129+cndonovan@users.noreply.github.com>
2023-04-21 10:05:04 -0700

    [ENHANCEMENT] Use abbreviations for time units
    Signed-off-by: Christine Donovan <2584129+cndonovan@users.noreply.github.com>

--- ui/components/src/model/units/units.test.ts
+++ ui/components/src/model/units/units.test.ts
@@ -11,61 +11,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { formatValue, UnitOptions } from './units';
+import { UnitOptions } from './units';
 
 export interface UnitTestCase {
   value: number;
   unit: UnitOptions;
   expected: string;
 }
-
-// TODO: Create test files for time units. Write more tests for time units.
-describe('formatValue', () => {
-  const tests: UnitTestCase[] = [
-    // Time
-    {
-      value: 8000,
-      unit: { kind: 'Milliseconds' },
-      expected: '8,000 milliseconds',
-    },
-    {
-      value: 200900,
-      unit: { kind: 'Seconds' },
-      expected: '200,900 seconds',
-    },
-    {
-      value: 300,
-      unit: { kind: 'Minutes' },
-      expected: '300 minutes',
-    },
-    {
-      value: 300,
-      unit: { kind: 'Hours' },
-      expected: '300 hours',
-    },
-    {
-      value: 300,
-      unit: { kind: 'Days' },
-      expected: '300 days',
-    },
-    {
-      value: 300,
-      unit: { kind: 'Weeks' },
-      expected: '300 weeks',
-    },
-    {
-      value: 300,
-      unit: { kind: 'Months' },
-      expected: '300 months',
-    },
-    {
-      value: 300,
-      unit: { kind: 'Years' },
-      expected: '300 years',
-    },
-  ];
-  it.each(tests)('returns $expected when $value formatted as $unit', (args: UnitTestCase) => {
-    const { value, unit, expected } = args;
-    expect(formatValue(value, unit)).toEqual(expected);
-  });
-});
