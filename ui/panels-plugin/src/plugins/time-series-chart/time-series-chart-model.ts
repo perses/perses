860bec2c74a456a5d9298871eaa701f19ec86db3
Christine Donovan <2584129+cndonovan@users.noreply.github.com>
2023-05-03 15:42:06 -0700

    Improve algorithm for picking y-axis minimum value
    Signed-off-by: Christine Donovan <2584129+cndonovan@users.noreply.github.com>

--- ui/panels-plugin/src/plugins/time-series-chart/time-series-chart-model.ts
+++ ui/panels-plugin/src/plugins/time-series-chart/time-series-chart-model.ts
@@ -137,7 +137,8 @@ export const PANEL_HEIGHT_LG_BREAKPOINT = 300;
 export const LEGEND_HEIGHT_SM = 40;
 export const LEGEND_HEIGHT_LG = 100;
 
-export const MIN_VALUE_PADDING_MULTIPLIER = 0.8;
+export const POSITIVE_MIN_VALUE_MULTIPLIER = 0.8;
+export const NEGATIVE_MIN_VALUE_MULTIPLIER = 1.2;
 
 /**
  * Creates an initial/empty options object for the TimeSeriesChartPanel.
