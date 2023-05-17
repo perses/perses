830d5ce8101ab9a1d4a6a93ce69063753d41bfbc
Steven Cobb <sjcobb.dev@gmail.com>
2023-05-16 23:46:23 -0400

    adjust saved default test cases
    Signed-off-by: Steven Cobb <sjcobb.dev@gmail.com>

--- ui/e2e/src/tests/saveDefaults.spec.ts
+++ ui/e2e/src/tests/saveDefaults.spec.ts
@@ -28,7 +28,6 @@ test.describe('Dashboard: Defaults', () => {
   });
 
   test('can update default dashboard duration', async ({ page, dashboardPage }) => {
-    const initialDashboardUrl = await page.url();
     // Change selected relative time range
     await dashboardPage.timePicker.click();
     await page.getByRole('option', { name: 'Last 6 hours' }).click();
@@ -60,9 +59,11 @@ test.describe('Dashboard: Defaults', () => {
     await dialogSaveButton.click();
 
     // Confirm correct default list and text variables are persisted
-    await page.goto(initialDashboardUrl);
+    await page.evaluate(() => {
+      window.location.search = '';
+    });
+    await dashboardPage.page.getByRole('button', { name: 'Refresh dashboard' }).click();
     await expect(page.url()).toContain('start=6h');
-    await expect(page.url()).toContain(TEXT_VARIABLE_UPDATED_VALUE);
     await expect(dashboardPage.timePicker).toContainText('Last 6 hours');
     await expect(dashboardPage.variableListItems).toContainText(['5m']);
   });
