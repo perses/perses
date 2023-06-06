41b7f2bbc8eca4106385ad1284fe3df917cfcc50
Shan Aminzadeh <shan.aminzadeh@chronosphere.io>
2023-06-06 08:39:00 -0400

    fix typo
    Signed-off-by: Shan Aminzadeh <shan.aminzadeh@chronosphere.io>

--- ui/dashboards/src/utils/variables.ts
+++ ui/dashboards/src/utils/variables.ts
@@ -13,7 +13,7 @@
 
 import { useTemplateVariableValues, parseTemplateVariables, replaceTemplateVariables } from '@perses-dev/plugin-system';
 
-// Convinience hook for replacing template variables in a string
+// Convenience hook for replacing template variables in a string
 export function useReplaceVariablesInString(str: string | null): string | null {
   const variablesInString = str ? parseTemplateVariables(str) : [];
   const variableValues = useTemplateVariableValues(variablesInString);
