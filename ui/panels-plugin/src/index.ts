18dc8b5da85cdc9eb7e874ec10411d4f638d3e60
Steven Cobb <sjcobb.dev@gmail.com>
2023-06-07 14:48:46 -0400

    [ENHANCEMENT] Export from each core panel plugin barrel file (#1229)
    Signed-off-by: Steven Cobb <sjcobb.dev@gmail.com>

--- ui/panels-plugin/src/index.ts
+++ ui/panels-plugin/src/index.ts
@@ -11,11 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { GaugeChart, GaugeChartDefinition } from './plugins/gauge-chart';
-import { TimeSeriesChart, TimeSeriesChartDefinition } from './plugins/time-series-chart';
-import { Markdown, MarkdownPanelDefinition } from './plugins/markdown';
-import { StatChart, StatChartDefinition } from './plugins/stat-chart';
-
-// Just export the plugins under the same name as the kinds they handle from the plugin.json
-export { TimeSeriesChart, GaugeChart, StatChart, Markdown };
-export type { TimeSeriesChartDefinition, GaugeChartDefinition, StatChartDefinition, MarkdownPanelDefinition };
+export * from './plugins/time-series-chart';
+export * from './plugins/gauge-chart';
+export * from './plugins/stat-chart';
+export * from './plugins/markdown';
