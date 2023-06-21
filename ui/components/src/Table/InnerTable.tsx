24b2ea9620780edec2f6b57e7666acdcbaae2458
Julie Pagano <julie.pagano@chronosphere.io>
2023-06-21 12:56:12 -0700

    [BUGFIX] avoid unnecessary horizontal scroll with table & legend (#1260)
    Signed-off-by: Julie Pagano <julie.pagano@chronosphere.io>

--- ui/components/src/Table/InnerTable.tsx
+++ ui/components/src/Table/InnerTable.tsx
@@ -32,7 +32,7 @@ const TABLE_DENSITY_CONFIG: Record<TableDensity, MuiTableProps['size']> = {
 };
 
 export const InnerTable = forwardRef<HTMLTableElement, InnerTableProps>(function InnerTable(
-  { density, width, ...otherProps },
+  { density, ...otherProps },
   ref
 ) {
   return (
@@ -42,7 +42,7 @@ export const InnerTable = forwardRef<HTMLTableElement, InnerTableProps>(function
       size={TABLE_DENSITY_CONFIG[density]}
       ref={ref}
       sx={{
-        width: width,
+        width: '100%',
       }}
     />
   );
