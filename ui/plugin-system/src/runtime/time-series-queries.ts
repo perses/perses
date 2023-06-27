d024d8518b32fd370a567dba2fb886dfae12b72b
Eunice Wong <eunicewong@live.com>
2023-06-27 10:49:58 -0400

    fix use time series queries return type
    Signed-off-by: Eunice Wong <eunicewong@live.com>

--- ui/plugin-system/src/runtime/time-series-queries.ts
+++ ui/plugin-system/src/runtime/time-series-queries.ts
@@ -119,13 +119,13 @@ export function useTimeSeriesQueries(definitions: TimeSeriesQueryDefinition[], o
   );
 
   return useQueries({
-    queries: definitions.map<QueryObserverOptions>((definition, idx) => {
+    queries: definitions.map((definition, idx) => {
       const plugin = pluginLoaderResponse[idx]?.data;
       const { queryEnabled, queryKey } = getQueryOptions({ plugin, definition, context });
       return {
         enabled: queryEnabled,
         queryKey: queryKey,
-        refetchInterval: context.refreshIntervalInMs > 0 ? context.refreshIntervalInMs : false,
+        refetchInterval: context.refreshIntervalInMs > 0 ? context.refreshIntervalInMs : undefined,
         queryFn: async () => {
           // Keep options out of query key so we don't re-run queries because suggested step changes
           const ctx: TimeSeriesQueryContext = { ...context, suggestedStepMs: options?.suggestedStepMs };
