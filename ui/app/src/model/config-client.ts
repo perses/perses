f79092658031e84144e07e47bdcc87563ab98b87
Augustin Husson <husson.augustin@gmail.com>
2023-10-20 10:45:18 +0200

    [IGNORE] update config interface in the UI app
    Signed-off-by: Augustin Husson <husson.augustin@gmail.com>

--- ui/app/src/model/config-client.ts
+++ ui/app/src/model/config-client.ts
@@ -29,8 +29,63 @@ export interface ConfigSchemasModel {
   interval: string;
 }
 
+export interface DatabaseFile {
+  folder: string;
+  extension: 'yaml' | 'json';
+}
+
+export interface TLSConfig {
+  ca?: string;
+  ca_file?: string;
+  cert?: string;
+  cert_file?: string;
+  key?: string;
+  key_file?: string;
+  server_name?: string;
+  insecure_skip_verify: boolean;
+  min_version: string;
+  max_version: string;
+}
+
+export interface DatabaseSQL {
+  tls_config?: TLSConfig;
+  user?: string;
+  password?: string;
+  password_file?: string;
+  net?: string;
+  addr?: string;
+  db_name?: string;
+  collation?: string;
+  max_allowed_packet?: number;
+  server_pub_key?: string;
+  timeout?: string;
+  read_timeout?: string;
+  write_timeout?: string;
+  allow_all_files: boolean;
+  allow_cleartext_passwords: boolean;
+  allow_fallback_to_plaintext: boolean;
+  allow_native_passwords: boolean;
+  allow_old_passwords: boolean;
+  check_conn_liveness: boolean;
+  client_found_rows: boolean;
+  columns_with_alias: boolean;
+  interpolate_params: boolean;
+  multi_statements: boolean;
+  parse_time: boolean;
+  reject_read_only: boolean;
+}
+
+export interface Database {
+  file?: DatabaseFile;
+  sql?: DatabaseSQL;
+}
+
 export interface ConfigModel {
   readonly: boolean;
+  activate_permission: boolean;
+  encryption_key?: string;
+  encryption_key_file?: string;
+  database: Database;
   schemas: ConfigSchemasModel;
   important_dashboards: DashboardSelector[];
   information: string;
