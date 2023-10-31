d95e76304f7f4807d499f921cd0732470cd5a696
Augustin Husson <husson.augustin@gmail.com>
2023-10-31 14:43:01 +0100

    fix running script
    Signed-off-by: Augustin Husson <husson.augustin@gmail.com>

--- scripts/api_backend_dev.sh
+++ scripts/api_backend_dev.sh
@@ -9,7 +9,11 @@ make build-api
 
 if [[ $1 == "--e2e" ]]; then
   # deactivate the permission because e2e tests doesn't support yet the JWT cookies
-  sed 's/activate_permission: true/activate_permission: false/g' ./dev/config.yaml
+  previous_file="./dev/config.previous.yaml"
+  config_file="./dev/config.yaml"
+  cp ${config_file} ${previous_file}
+  sed 's/activate_permission: true/activate_permission: false/g' ${previous_file} >${config_file}
+  rm ${previous_file}
 fi
 
 # Run backend server
