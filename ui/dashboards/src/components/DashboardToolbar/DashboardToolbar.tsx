9f2a3268f8dac8f57ae0e34eee4aa4611cf7fe99
Antoine THEBAUD <antoine.thebaud@yahoo.fr>
2023-12-13 09:03:42 +0100

    [ENHANCEMENT] Reposition the dashboard toolbar buttons to have less difference between read & edit modes
    Signed-off-by: Antoine THEBAUD <antoine.thebaud@yahoo.fr>

--- ui/dashboards/src/components/DashboardToolbar/DashboardToolbar.tsx
+++ ui/dashboards/src/components/DashboardToolbar/DashboardToolbar.tsx
@@ -61,10 +61,15 @@ export const DashboardToolbar = (props: DashboardToolbarProps) => {
 
   return (
     <>
-      {isEditMode ? (
-        <Stack spacing={1} data-testid={testId}>
-          <Box px={2} py={1.5} display="flex" sx={{ backgroundColor: (theme) => theme.palette.primary.main + '30' }}>
-            {dashboardTitle}
+      <Stack spacing={1} data-testid={testId}>
+        <Box
+          px={2}
+          py={1.5}
+          display="flex"
+          sx={{ backgroundColor: (theme) => theme.palette.primary.main + (isEditMode ? '30' : '0') }}
+        >
+          {dashboardTitle}
+          {isEditMode ? (
             <Stack direction="row" gap={1} ml="auto">
               {isReadonly && (
                 <Alert severity={'warning'} sx={{ backgroundColor: 'transparent', padding: 0 }}>
@@ -76,60 +81,49 @@ export const DashboardToolbar = (props: DashboardToolbarProps) => {
                 Cancel
               </Button>
             </Stack>
-          </Box>
-          <Box
-            sx={{
-              display: 'flex',
-              width: '100%',
-              alignItems: 'start',
-              padding: (theme) => theme.spacing(1, 2, 0, 2),
-            }}
-          >
-            <ErrorBoundary FallbackComponent={ErrorAlert}>
-              <DashboardStickyToolbar
-                initialVariableIsSticky={initialVariableIsSticky}
-                sx={{
-                  backgroundColor: ({ palette }) => palette.background.default,
-                }}
-              />
-            </ErrorBoundary>
-            <Stack ml="auto" direction="row" flexWrap={isBiggerThanLg ? 'nowrap' : 'wrap-reverse'} justifyContent="end">
+          ) : (
+            <>
+              {isBiggerThanSm && (
+                <Stack direction="row" gap={1} ml="auto">
+                  <EditButton onClick={onEditButtonClick} />
+                </Stack>
+              )}
+            </>
+          )}
+        </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            width: '100%',
+            alignItems: 'start',
+            padding: (theme) => theme.spacing(1, 2, 0, 2),
+          }}
+        >
+          <ErrorBoundary FallbackComponent={ErrorAlert}>
+            <DashboardStickyToolbar
+              initialVariableIsSticky={initialVariableIsSticky}
+              sx={{
+                backgroundColor: ({ palette }) => palette.background.default,
+              }}
+            />
+          </ErrorBoundary>
+          <Stack ml="auto" direction="row" flexWrap={isBiggerThanLg ? 'nowrap' : 'wrap-reverse'} justifyContent="end">
+            {isEditMode && (
               <Stack direction="row" spacing={1} ml={1} whiteSpace="nowrap">
                 <EditVariablesButton />
                 <EditDatasourcesButton />
                 <AddPanelButton />
                 <AddGroupButton />
               </Stack>
-              <Stack direction="row" spacing={1} ml={1}>
-                <TimeRangeControls />
-                <DownloadButton />
-                <EditJsonButton />
-              </Stack>
-            </Stack>
-          </Box>
-        </Stack>
-      ) : (
-        <Stack gap={1} mx={2} mt={1.5} data-testid={testId}>
-          <Box sx={{ display: 'flex', width: '100%' }}>
-            {dashboardTitle}
-            <Stack direction="row" spacing={1} marginLeft="auto">
+            )}
+            <Stack direction="row" spacing={1} ml={1}>
               <TimeRangeControls />
               <DownloadButton />
-              {isBiggerThanSm && <EditButton onClick={onEditButtonClick} />}
+              {isEditMode && <EditJsonButton />}
             </Stack>
-          </Box>
-          <Box mt={1}>
-            <ErrorBoundary FallbackComponent={ErrorAlert}>
-              <DashboardStickyToolbar
-                initialVariableIsSticky={initialVariableIsSticky}
-                sx={{
-                  backgroundColor: ({ palette }) => palette.background.default,
-                }}
-              />
-            </ErrorBoundary>
-          </Box>
-        </Stack>
-      )}
+          </Stack>
+        </Box>
+      </Stack>
     </>
   );
 };
