3dc47b6336f1538cfde3485ddd32339ab0baab2a
Antoine THEBAUD <antoine.thebaud@yahoo.fr>
2024-01-23 22:40:29 +0100

    [ENHANCEMENT] Support referencing variables in 'Min step' field
    Signed-off-by: Antoine THEBAUD <antoine.thebaud@yahoo.fr>

--- ui/prometheus-plugin/src/plugins/prometheus-time-series-query/get-time-series-data.ts
+++ ui/prometheus-plugin/src/plugins/prometheus-time-series-query/get-time-series-data.ts
@@ -13,6 +13,7 @@
 
 import {
   DatasourceSpec,
+  DurationString,
   formatDuration,
   msToPrometheusDuration,
   Notice,
@@ -49,7 +50,11 @@ export const getTimeSeriesData: TimeSeriesQueryPlugin<PrometheusTimeSeriesQueryS
     milliseconds(parseDurationString(datasource.plugin.spec.scrapeInterval ?? DEFAULT_SCRAPE_INTERVAL)) / 1000
   );
 
-  const minStep = getDurationStringSeconds(spec.minStep) ?? datasourceScrapeInterval;
+  const minStep =
+    getDurationStringSeconds(
+      // resolve any variable that may have been provided
+      replaceTemplateVariables(spec.minStep as string, context.variableState) as DurationString
+    ) ?? datasourceScrapeInterval;
   const timeRange = getPrometheusTimeRange(context.timeRange);
   const step = getRangeStep(timeRange, minStep, undefined, context.suggestedStepMs); // TODO: resolution
 
