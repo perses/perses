ebea24dc1248ff0cd3f341a3cf8dee45c844dc8d
Gabriel Bernal <gbernal@redhat.com>
2024-06-11 00:32:09 +0200

    [FEATURE] add plugin runtime
    Signed-off-by: Gabriel Bernal <gbernal@redhat.com>

---
+++ ui/plugin-runtime/src/PluginLoader.tsx
@@ -0,0 +1,66 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// Copyright 2024 The Perses Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { useEffect, useState } from 'react';
+import { PersesPlugin, PersesPluginModule } from './PersesPlugin.types';
+import { usePluginRuntime } from './PluginRuntime';
+
+interface PluginLoaderProps<P> {
+  plugin: PersesPlugin;
+  props?: P;
+}
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+function PluginContainer<P>({ pluginFn, props }: { pluginFn: Function; props: P }) {
+  return pluginFn(props);
+}
+
+export function PluginLoader<P>({ plugin, props }: PluginLoaderProps<P>) {
+  const { load } = usePluginRuntime({ moduleName: plugin.moduleName, baseURL: plugin.baseURL });
+  const [pluginModule, setPluginModule] = useState<PersesPluginModule | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    setError(null);
+
+    load(plugin.name)
+      .then((module) => {
+        setPluginModule(module);
+      })
+      .catch((error) => {
+        setPluginModule(null);
+        console.error(`PluginLoader: Error loading plugin ${plugin.name}:`, error);
+        setError(new Error(`PluginLoader: Error loading plugin ${plugin.name}`));
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [plugin.name]);
+
+  if (error) {
+    throw error;
+  }
+
+  if (!pluginModule) {
+    return null;
+  }
+
+  if (!pluginModule.default) {
+    throw new Error('PluginLoader: Plugin module does not have a default export');
+  }
+
+  if (typeof pluginModule.default !== 'function') {
+    throw new Error('PluginLoader: Plugin module default export is not a function');
+  }
+
+  return <PluginContainer pluginFn={pluginModule.default} props={props} />;
+}
