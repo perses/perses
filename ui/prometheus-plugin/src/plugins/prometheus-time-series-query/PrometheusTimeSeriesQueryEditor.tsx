a9a598fbd536e78a18d0ed9e4d7930a748114b9d
ZonaHe <zonahe@qq.com>
2024-10-24 18:05:32 +0800

    [ENHANCEMENT]: Support custom requestHeaders for autocompletion of promql codemirror editor
    Signed-off-by: ZonaHe <zonahe@qq.com>

--- ui/prometheus-plugin/src/plugins/prometheus-time-series-query/PrometheusTimeSeriesQueryEditor.tsx
+++ ui/prometheus-plugin/src/plugins/prometheus-time-series-query/PrometheusTimeSeriesQueryEditor.tsx
@@ -14,6 +14,8 @@
 import { produce } from 'immer';
 import { DatasourceSelect, DatasourceSelectProps, useDatasource, useDatasourceClient } from '@perses-dev/plugin-system';
 import { FormControl, InputLabel, Stack, TextField } from '@mui/material';
+import { CompleteConfiguration } from '@prometheus-io/codemirror-promql';
+import { HTTPProxy } from '@perses-dev/core';
 import {
   DEFAULT_PROM,
   DurationString,
@@ -41,8 +43,17 @@ export function PrometheusTimeSeriesQueryEditor(props: PrometheusTimeSeriesQuery
   const datasourceSelectLabelID = `prom-datasource-label-${selectedDatasource.name || 'default'}`;
 
   const { data: client } = useDatasourceClient<PrometheusClient>(selectedDatasource);
-  const promURL = client?.options.datasourceUrl;
   const { data: datasourceResource } = useDatasource(selectedDatasource);
+
+  const promURL =
+    (datasourceResource?.plugin.spec as PrometheusDatasourceSpec).directUrl ?? client?.options.datasourceUrl;
+  let completeConfig: CompleteConfiguration;
+  if (datasourceResource && (datasourceResource.plugin.spec.proxy as HTTPProxy)?.spec.headers) {
+    const promHeaders = (datasourceResource.plugin.spec.proxy as HTTPProxy).spec.headers;
+    completeConfig = { remote: { url: promURL, requestHeaders: promHeaders } };
+  } else {
+    completeConfig = { remote: { url: promURL } };
+  }
 
   const { query, handleQueryChange, handleQueryBlur } = useQueryState(props);
   const { format, handleFormatChange, handleFormatBlur } = useFormatState(props);
@@ -80,7 +91,7 @@ export function PrometheusTimeSeriesQueryEditor(props: PrometheusTimeSeriesQuery
         />
       </FormControl>
       <PromQLEditor
-        completeConfig={{ remote: { url: promURL } }}
+        completeConfig={completeConfig}
         value={query}
         onChange={handleQueryChange}
         onBlur={handleQueryBlur}
