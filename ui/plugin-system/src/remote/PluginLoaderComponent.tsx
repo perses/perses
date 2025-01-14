ca449c7aed44ea32cb9c2cc73cd28759df45f33f
Gabriel Bernal <gbernal@redhat.com>
2025-01-14 13:58:59 +0100

    [FEATURE] add plugin runtime into plugin system package, fix react query shared package
    Signed-off-by: Gabriel Bernal <gbernal@redhat.com>

--- ui/plugin-system/src/remote/PluginLoaderComponent.tsx
+++ ui/plugin-system/src/remote/PluginLoaderComponent.tsx
@@ -19,6 +19,7 @@ import { usePluginRuntime } from './PluginRuntime';
 interface PluginLoaderProps<P> {
   plugin: PersesPlugin;
   props?: P;
+  field?: string;
 }
 
 // eslint-disable-next-line @typescript-eslint/ban-types
@@ -26,7 +27,7 @@ function PluginContainer<P>({ pluginFn, props }: { pluginFn: Function; props: P 
   return pluginFn(props);
 }
 
-export function PluginLoader<P>({ plugin, props }: PluginLoaderProps<P>): JSX.Element | null {
+export function PluginLoaderComponent<P>({ plugin, props, field }: PluginLoaderProps<P>): JSX.Element | null {
   const { loadPlugin } = usePluginRuntime({ plugin });
   const [pluginModule, setPluginModule] = useState<RemotePluginModule | null>(null);
   const [error, setError] = useState<Error | null>(null);
@@ -44,8 +45,13 @@ export function PluginLoader<P>({ plugin, props }: PluginLoaderProps<P>): JSX.El
       })
       .catch((error) => {
         setPluginModule(null);
-        console.error(`PluginLoader: Error loading plugin ${plugin.name} from module ${plugin.moduleName}:`, error);
-        setError(new Error(`PluginLoader: Error loading plugin ${plugin.name} from module ${plugin.moduleName}`));
+        console.error(
+          `PluginLoaderComponent: Error loading plugin ${plugin.name} from module ${plugin.moduleName}:`,
+          error
+        );
+        setError(
+          new Error(`PluginLoaderComponent: Error loading plugin ${plugin.name} from module ${plugin.moduleName}`)
+        );
       });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [name]);
@@ -58,14 +64,18 @@ export function PluginLoader<P>({ plugin, props }: PluginLoaderProps<P>): JSX.El
     return null;
   }
 
-  const pluginFunction = pluginModule[plugin.name];
+  let pluginFunction = pluginModule[plugin.name];
+
+  if (field && pluginFunction && typeof pluginFunction === 'object' && field in pluginFunction) {
+    pluginFunction = (pluginFunction as Record<string, unknown>)[field];
+  }
 
   if (!pluginFunction) {
-    throw new Error(`PluginLoader: Plugin module does not have a ${plugin.name} export`);
+    throw new Error(`PluginLoaderComponent: Plugin module ${plugin.moduleName} does not have a ${plugin.name} export`);
   }
 
   if (typeof pluginFunction !== 'function') {
-    throw new Error(`PluginLoader: Plugin ${plugin.name} export is not a function`);
+    throw new Error(`PluginLoaderComponent: Plugin ${plugin.name} export is not a function`);
   }
 
   // make sure to re mount the plugin when changes, to avoid mismatch in hooks ordering when re rendering
