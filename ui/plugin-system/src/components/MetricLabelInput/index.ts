01d97369dc6b5963ba61a648e70327c728fdcfd9
Alexander Belyakin <21216343+abelyakin@users.noreply.github.com>
2025-01-23 16:07:30 +0300

    [FEATURE] Label metrics in stat chart (#2574)
    * Label metrics
    
    Signed-off-by: Alexander Belyakin <21216343+abelyakin@users.noreply.github.com>
    
    * add regex to label metrics
    
    Signed-off-by: Alexander Belyakin <21216343+abelyakin@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Alexander Belyakin <21216343+abelyakin@users.noreply.github.com>

--- ui/plugin-system/src/components/MetricLabelInput/index.ts
+++ ui/plugin-system/src/components/MetricLabelInput/index.ts
@@ -11,24 +11,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package model
-
-import (
-	"github.com/perses/perses/cue/schemas/common"
-)
-
-kind: "StatChart"
-spec: close({
-	calculation:    common.#calculation
-	metricLabel?:   common.#metricLabel
-	format?:        common.#format
-	thresholds?:    common.#thresholds
-	sparkline?:     #sparkline
-	valueFontSize?: number
-	mappings?: [...common.#mappings]
-
-	#sparkline: {
-		color?: string
-		width?: number
-	}
-})
+export * from './MetricLabelInput';
