6d50434a91e98621192bb98f6b864a66137f8492
Antoine THEBAUD <antoine.thebaud@yahoo.fr>
2025-02-14 10:40:21 +0100

    improve TestNewFromSchema
    Signed-off-by: Antoine THEBAUD <antoine.thebaud@yahoo.fr>

--- internal/api/discovery/cuetils/cuetils_test.go
+++ internal/api/discovery/cuetils/cuetils_test.go
@@ -14,14 +14,13 @@
 package cuetils
 
 import (
-	"fmt"
 	"path/filepath"
 	"sort"
 	"testing"
 
 	"cuelang.org/go/cue"
 	"cuelang.org/go/cue/cuecontext"
-	"cuelang.org/go/cue/load"
+	"github.com/perses/perses/internal/api/plugin/schema"
 	"github.com/perses/perses/internal/test"
 	"github.com/perses/perses/pkg/model/api/config"
 	"github.com/perses/perses/pkg/model/api/v1/common"
@@ -127,13 +126,11 @@ func TestNewFromSchema(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			v, err := buildCUESchema(tt.schema)
 			if err != nil {
-				t.Error(err)
-				return
+				t.Fatal(err)
 			}
 			trees, err := NewFromSchema(v)
 			if err != nil {
-				t.Errorf("NewFromSchema() error = %v", err)
-				return
+				t.Fatalf("NewFromSchema() error = %v", err)
 			}
 			sortNodes(trees)
 			assert.Equal(t, tt.expected, trees)
@@ -169,19 +166,16 @@ spec:
 		t.Run(tt.name, func(t *testing.T) {
 			v, err := buildCUESchema(tt.schema)
 			if err != nil {
-				t.Error(err)
-				return
+				t.Fatal(err)
 			}
 			trees, err := NewFromSchema(v)
 			if err != nil {
-				t.Errorf("NewFromSchema() error = %v", err)
-				return
+				t.Fatalf("NewFromSchema() error = %v", err)
 			}
 			sortNodes(trees)
 			plugin, err := BuildPluginAndInjectProxy(trees, tt.proxy)
 			if err != nil {
-				t.Errorf("BuildPluginAndInjectProxy() error = %v", err)
-				return
+				t.Fatalf("BuildPluginAndInjectProxy() error = %v", err)
 			}
 			d, err := yaml.Marshal(plugin)
 			if err != nil {
@@ -194,13 +188,11 @@ spec:
 
 func buildCUESchema(path string) (cue.Value, error) {
 	ctx := cuecontext.New()
-	buildInstances := load.Instances([]string{}, &load.Config{Dir: path, Package: "model"})
-	// we strongly assume that only 1 buildInstance should be returned, otherwise we skip it
-	if len(buildInstances) != 1 {
-		return cue.Value{}, fmt.Errorf("Plugin will not be loaded: The number of build instances is != 1")
+	schemaInstance, err := schema.LoadSchemaInstance(path, "model")
+	if err != nil {
+		return cue.Value{}, err
 	}
-	buildInstance := buildInstances[0]
-	return ctx.BuildInstance(buildInstance), nil
+	return ctx.BuildInstance(schemaInstance), nil
 }
 
 func sortNodes(nodes []*Node) {
