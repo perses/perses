903328281c2414a405d35e9161698c4dfd4626ad
Augustin Husson <husson.augustin@gmail.com>
2025-03-05 18:59:45 +0100

    [BUGFIX] Fix the interface impl of the datasource discovery (#2714)
    Signed-off-by: Augustin Husson <husson.augustin@gmail.com>

--- internal/api/discovery/kubernetes/kubernetes.go
+++ internal/api/discovery/kubernetes/kubernetes.go
@@ -90,7 +90,7 @@ type discovery struct {
 	name      string
 }
 
-func (d *discovery) Execute(_ context.Context, _ context.Context) error {
+func (d *discovery) Execute(_ context.Context, _ context.CancelFunc) error {
 	decodedSchema, err := d.decodeSchema()
 	if err != nil {
 		logrus.WithError(err).Error("failed to decode schema")
@@ -103,6 +103,10 @@ func (d *discovery) Execute(_ context.Context, _ context.Context) error {
 	}
 	d.svc.Apply(result)
 	return nil
+}
+
+func (d *discovery) String() string {
+	return fmt.Sprintf("datasource discovery %q", d.name)
 }
 
 func (d *discovery) decodeSchema() ([]*cuetils.Node, error) {
