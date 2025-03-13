daeaabadcf8dedbe00f5dd0925fae9077fcce54d
Augustin Husson <husson.augustin@gmail.com>
2025-03-13 11:59:24 +0100

    increase playwright test timeout
    Signed-off-by: Augustin Husson <husson.augustin@gmail.com>

--- ui/e2e/src/config/base.playwright.config.ts
+++ ui/e2e/src/config/base.playwright.config.ts
@@ -21,7 +21,7 @@ import { PlaywrightTestConfig, devices } from '@playwright/test';
 const config: PlaywrightTestConfig = {
   testDir: '../tests',
   /* Maximum time one test can run for. */
-  timeout: 60 * 1000,
+  timeout: 120 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
