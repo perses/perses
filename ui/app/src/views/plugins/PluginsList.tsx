55188eb473885f0627a8be4e403c314befb0f14d
Akshay Iyyadurai Balasundaram <akshay.iyyadurai.balasundaram@sap.com>
2025-04-02 13:22:19 +0200

    feat(plugins): list of plugins
    Signed-off-by: Akshay Iyyadurai Balasundaram <akshay.iyyadurai.balasundaram@sap.com>

--- ui/app/src/views/plugins/PluginsList.tsx
+++ ui/app/src/views/plugins/PluginsList.tsx
@@ -1,5 +1,7 @@
 import { useSnackbar } from '@perses-dev/components';
 import { PluginModuleResource } from '@perses-dev/plugin-system';
+import { Box, Card, CardContent, Typography, Paper, useTheme } from '@mui/material';
+import Grid from '@mui/material/Grid2';
 import { ReactElement, useEffect, useState } from 'react';
 
 export function PluginsList(): ReactElement {
@@ -7,6 +9,7 @@ export function PluginsList(): ReactElement {
   const [error, setError] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const { exceptionSnackbar } = useSnackbar();
+  const theme = useTheme();
 
   useEffect(() => {
     const fetchPlugins = async (): Promise<void> => {
@@ -43,8 +46,39 @@ export function PluginsList(): ReactElement {
 
   // Render the list of plugins
   return (
-    <div>
-      <h2>Plugins List</h2>
+    <Box>
+      <Paper elevation={0} sx={{ p: 3, mb: 2 }}>
+        <Typography variant="body1">Available plugins in this Perses instance</Typography>
+      </Paper>
+
+      <Grid container spacing={3}>
+        {plugins.map((plugin) => (
+          <Grid size={{ xs: 12, sm: 6, md: 4, lg: 3 }} key={plugin.metadata.name}>
+            <Card
+              elevation={1}
+              sx={{
+                height: '100%',
+                display: 'flex',
+                flexDirection: 'column',
+                transition: 'transform 0.2s box-shadow 0.2s',
+                '&!hover': {
+                  transform: 'translateY(-4px)',
+                  boxShadow: theme.shadows[4],
+                },
+              }}
+            >
+              <CardContent sx={{ flexGrow: 1 }}>
+                <Typography variant="h3" component="div" gutterBottom>
+                  {plugin?.metadata?.name}
+                </Typography>
+                <Typography color="text.secondary" sx={{ mb: 1 }}>
+                  Version {plugin.metadata.version}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
       <ul>
         {plugins.map((plugin) => (
           <li key={plugin?.metadata?.name}>
@@ -52,6 +86,6 @@ export function PluginsList(): ReactElement {
           </li>
         ))}
       </ul>
-    </div>
+    </Box>
   );
 }
