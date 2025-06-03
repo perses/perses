9d6f10fa7aa5794201fccfc65e45c6d7a2d9bd0f
Or Reuben <110329402+OrReuben@users.noreply.github.com>
2025-06-03 12:10:22 +0300

    [BUGFIX] ListVariable Infinite Re-render (#2939)
    * Fix
    
    Signed-off-by: OrReuben <orreuven1243@gmail.com>
    
    * Tests
    
    Signed-off-by: OrReuben <orreuven1243@gmail.com>
    
    * license
    
    Signed-off-by: OrReuben <orreuven1243@gmail.com>
    
    ---------
    
    Signed-off-by: OrReuben <orreuven1243@gmail.com>
    Signed-off-by: Aditya Pimpalkar <adipimpalkar@gmail.com>

--- ui/plugin-system/src/test/render-hook.tsx
+++ ui/plugin-system/src/test/render-hook.tsx
@@ -11,19 +11,18 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { render, RenderOptions, RenderResult } from '@testing-library/react';
-import { ReactNode } from 'react';
+import { renderHook, RenderHookOptions, RenderHookResult } from '@testing-library/react';
 import { ContextOptions, getTestContextWrapper } from './utils';
 
 /**
- * Test helper to render a React component with some common app-level providers, as well as the PluginRegistry
- * wrapped around it.
+ * Test helper to render a React hook with common app-level providers, including the PluginRegistry,
+ * wrapped around it. Useful for testing hooks that rely on context such as QueryClientProvider or PluginRegistry.
  */
-export function renderWithContext(
-  ui: ReactNode,
-  renderOptions?: Omit<RenderOptions, 'queries'>,
-  contextOptions?: ContextOptions
-): RenderResult {
-  const Wrapper = getTestContextWrapper(contextOptions);
-  return render(<Wrapper>{ui}</Wrapper>, renderOptions);
+export function renderHookWithContext<TProps, TResult>(
+  callback: (props: TProps) => TResult,
+  contextOptions?: ContextOptions,
+  renderHookOptions?: Omit<RenderHookOptions<TProps>, 'wrapper'>
+): RenderHookResult<TResult, TProps> {
+  const wrapper = getTestContextWrapper(contextOptions);
+  return renderHook(callback, { wrapper, ...renderHookOptions });
 }
