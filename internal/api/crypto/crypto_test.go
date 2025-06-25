d3257a44ad348aa9124b308c9a32611f5837a7e1
Aditya Pimpalkar <adipimpalkar@gmail.com>
2025-06-25 16:27:32 +0100

    fix: go lint and license header
    Signed-off-by: Aditya Pimpalkar <adipimpalkar@gmail.com>

--- internal/api/crypto/crypto_test.go
+++ internal/api/crypto/crypto_test.go
@@ -1,3 +1,16 @@
+// Copyright 2025 The Perses Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package crypto
 
 import (
@@ -20,7 +33,8 @@ func TestEncryptDecryptUsingCFB(t *testing.T) {
 	}
 	assert.NoError(t, err)
 	assert.NotEmpty(t, spec.BasicAuth.Password)
-	cfbEncryptedPassword := "Bm11iHDx3AL966MEKBjQrL_AN8pzFqnRtluw"
+	// this is just an encrypted string of password123 which I printed out on runtime. ONLY for testing purpose
+	cfbEncryptedPassword := "Bm11iHDx3AL966MEKBjQrL_AN8pzFqnRtluw" //nolint: gosec
 	spec.BasicAuth.Password = cfbEncryptedPassword
 
 	err = crypto.Decrypt(spec)
