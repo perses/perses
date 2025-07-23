73dbbc36e3a6cf406750bf41a828eed1b887a509
Coleen Iona Quadros <coleen.quadros27@gmail.com>
2025-07-23 14:32:27 +0200

    [ENHANCEMENT] make `unit` field in `format` optional (#3035)
    * make unit field in format optional
    
    Signed-off-by: Coleen Iona Quadros <coleen.quadros27@gmail.com>
    
    * make unit as pointer
    
    Signed-off-by: Coleen Iona Quadros <coleen.quadros27@gmail.com>
    
    * ui changes
    
    Signed-off-by: Coleen Iona Quadros <coleen.quadros27@gmail.com>
    
    * fix tests and lint
    
    Signed-off-by: Coleen Iona Quadros <coleen.quadros27@gmail.com>
    
    * [IGNORE] refactor types and unit fallbacks
    
    Signed-off-by: Gabriel Bernal <gbernal@redhat.com>
    
    ---------
    
    Signed-off-by: Coleen Iona Quadros <coleen.quadros27@gmail.com>
    Signed-off-by: Gabriel Bernal <gbernal@redhat.com>
    Co-authored-by: Gabriel Bernal <gbernal@redhat.com>

--- ui/core/src/model/units/units.ts
+++ ui/core/src/model/units/units.ts
@@ -96,16 +96,17 @@ export function formatValue(value: number, formatOptions?: FormatOptions): strin
 }
 
 export function getUnitConfig(formatOptions: FormatOptions): UnitConfig {
-  return UNIT_CONFIG[formatOptions.unit];
+  const unit = formatOptions.unit ?? 'decimal';
+  return UNIT_CONFIG[unit];
 }
 
 export function getUnitGroup(formatOptions: FormatOptions): UnitGroup {
-  return getUnitConfig(formatOptions).group;
+  return getUnitConfig(formatOptions).group ?? 'Decimal';
 }
 
 export function getUnitGroupConfig(formatOptions: FormatOptions): UnitGroupConfig {
   const unitConfig = getUnitConfig(formatOptions);
-  return UNIT_GROUP_CONFIG[unitConfig.group];
+  return UNIT_GROUP_CONFIG[unitConfig.group ?? 'Decimal'];
 }
 
 // Type guards
