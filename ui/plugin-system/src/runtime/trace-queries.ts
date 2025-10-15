116e8c27a201ba0163dc03d805b4cc2fc7e7d304
Andreas Gerstmayr <agerstmayr@redhat.com>
2025-10-15 16:10:11 +0200

    Remove unused imports
    Signed-off-by: Andreas Gerstmayr <agerstmayr@redhat.com>

--- ui/plugin-system/src/runtime/trace-queries.ts
+++ ui/plugin-system/src/runtime/trace-queries.ts
@@ -11,8 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { getUnixTime } from 'date-fns';
-import { QueryDefinition, UnknownSpec, AbsoluteTimeRange, TraceData } from '@perses-dev/core';
+import { QueryDefinition, UnknownSpec, TraceData } from '@perses-dev/core';
 import { QueryKey, useQueries, UseQueryResult } from '@tanstack/react-query';
 import { TraceQueryContext, TraceQueryPlugin } from '../model';
 import { useDatasourceStore } from './datasources';
