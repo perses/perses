af0b9e95752206bc3c7ee44333cac6063bb006e3
Seyed Mahmoud SHAHROKNI <seyedmahmoud.shahrokni@amadeus.com>
2025-10-27 16:26:28 +0100

    [BUGFIX] Save the refresh interval chnages from UI
    Signed-off-by: Seyed Mahmoud SHAHROKNI <seyedmahmoud.shahrokni@amadeus.com>
    
    Signed-off-by: Seyed Mahmoud SHAHROKNI <seyedmahmoud.shahrokni@amadeus.com>
    
    Signed-off-by: Seyed Mahmoud SHAHROKNI <seyedmahmoud.shahrokni@amadeus.com>
    
    Signed-off-by: Seyed Mahmoud SHAHROKNI <seyedmahmoud.shahrokni@amadeus.com>
    
    Signed-off-by: Seyed Mahmoud SHAHROKNI <seyedmahmoud.shahrokni@amadeus.com>

--- ui/plugin-system/src/components/TimeRangeControls/TimeRangeControls.tsx
+++ ui/plugin-system/src/components/TimeRangeControls/TimeRangeControls.tsx
@@ -195,7 +195,16 @@ export function TimeRangeControls({
         <InfoTooltip description={TOOLTIP_TEXT.refreshInterval}>
           <RefreshIntervalPicker
             timeOptions={DEFAULT_REFRESH_INTERVAL_OPTIONS}
-            value={refreshInterval}
+            value={
+              /* TODO: There is a bug here which should be fixed in a proper way. (This is only a quick remedy)
+                 display: 1m has the pastDuration of 60s. Initially (if the persisted value is 1m) when the page is loaded, instead of 60s, 1m is passed down.              
+                 This only happens for 1m, because for other items the display and the pastDuration are the same.  Example 30s-30s
+                 HERE The value MUST always be pastDuration, otherwise the component would not work as expected. 
+              */
+              DEFAULT_REFRESH_INTERVAL_OPTIONS.some((i) => i.value.pastDuration === refreshInterval)
+                ? refreshInterval
+                : DEFAULT_REFRESH_INTERVAL_OPTIONS.find((i) => i.display === refreshInterval)?.value.pastDuration
+            }
             onChange={handleRefreshIntervalChange}
             height={height}
           />
