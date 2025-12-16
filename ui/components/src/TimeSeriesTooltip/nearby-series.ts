215ed5f1d24a907bb00c410c069e6ea62ed8999d
Simon Olander <simon.olander@sap.com>
2025-12-16 14:04:32 +0100

    [FIX]: removes mathjs dependency for pow to use a simple ** operator instead
    ---------
    
    Signed-off-by: Simon Olander (simon.olander@sap.com)

--- ui/components/src/TimeSeriesTooltip/nearby-series.ts
+++ ui/components/src/TimeSeriesTooltip/nearby-series.ts
@@ -17,7 +17,6 @@ import { formatValue, TimeSeriesValueTuple, FormatOptions, TimeSeries } from '@p
 import { EChartsDataFormat, OPTIMIZED_MODE_SERIES_LIMIT, TimeChartSeriesMapping, DatapointInfo } from '../model';
 import { batchDispatchNearbySeriesActions, getPointInGrid, getClosestTimestamp } from '../utils';
 import { CursorCoordinates, CursorData, EMPTY_TOOLTIP_DATA } from './tooltip-model';
-import {pow} from 'mathjs';
 // increase multipliers to show more series in tooltip
 export const INCREASE_NEARBY_SERIES_MULTIPLIER = 5.5; // adjusts how many series show in tooltip (higher == more series shown)
 export const DYNAMIC_NEARBY_SERIES_MULTIPLIER = 30; // used for adjustment after series number divisor
@@ -47,8 +46,7 @@ export function checkforNearbyTimeSeries(
   pointInGrid: number[],
   yBuffer: number,
   chart: EChartsInstance,
-  format?: FormatOptions,
-  logBase?: number,
+  format?: FormatOptions
 ): NearbySeriesArray {
   const currentNearbySeriesData: NearbySeriesArray = [];
   const cursorX: number | null = pointInGrid[0] ?? null;
@@ -340,19 +338,19 @@ export function getNearbySeriesData({
     const yAxisScale = chartModel.getComponent('yAxis').axis.scale;
     const isLogScale = yAxisScale.type === 'log';
     let yInterval = yAxisScale._interval;
-    
+
     // For logarithmic scales, convert the log interval to actual data range
     if (isLogScale && yAxisScale.base) {
       const logBase = yAxisScale.base;
       const extent = yAxisScale._extent;
       // Calculate actual data range from log extent
       // extent is in log space (e.g., [0, 2] for 10^0 to 10^2)
-      const actualMin = Math.pow(logBase, extent[0]);
-      const actualMax = Math.pow(logBase, extent[1]);
+      const actualMin = logBase ** extent[0];
+      const actualMax = logBase ** extent[1];
       // Use a fraction of the actual range as the interval
       yInterval = (actualMax - actualMin) / 100;
     }
-    
+
     const totalSeries = data.length;
     const yBuffer = getYBuffer({ yInterval, totalSeries, showAllSeries });
     return checkforNearbyTimeSeries(data, seriesMapping, pointInGrid, yBuffer, chart, format);
