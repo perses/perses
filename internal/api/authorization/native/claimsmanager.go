e6ad6b0ae51776fb0418822f5655f8f9f8b1bd96
gfierksap <gabor.fierka@sap.com>
2026-03-24 07:59:00 +0100

    added claims caching mechanism
    Signed-off-by: gfierksap <gabor.fierka@sap.com>

--- internal/api/authorization/native/claimsmanager.go
+++ internal/api/authorization/native/claimsmanager.go
@@ -18,6 +18,8 @@ import (
 	"encoding/json"
 	"net/http"
 	"strings"
+	"sync"
+	"time"
 
 	"github.com/jmespath/go-jmespath"
 	"github.com/labstack/echo/v4"
@@ -26,11 +28,91 @@ import (
 
 const CookieName = "persistedClaims"
 
+// claims cache
+type entryValue struct {
+	cookie string
+	claims map[string]any
+}
+
+type entry struct {
+	value  entryValue
+	expiry time.Time
+}
+
+func (e entry) isExpired() bool {
+	return time.Now().After(e.expiry)
+}
+
+type Cache struct {
+	// items is a map where the key is the username, and entry values are the cookie and claims
+	// when a new session is created / cookie is refreshed, cache entry is redone
+	items map[string]entry
+	mu    sync.Mutex
+	ttl   time.Duration
+}
+
+func CreateCache() *Cache {
+	c := &Cache{
+		items: map[string]entry{},
+		ttl:   10 * time.Minute,
+	}
+
+	go func() {
+		for range time.Tick(10 * time.Second) {
+			c.mu.Lock()
+
+			for username, entry := range c.items {
+				if entry.isExpired() {
+					delete(c.items, username)
+				}
+			}
+			c.mu.Unlock()
+		}
+	}()
+
+	return c
+}
+
+func (c *Cache) Set(username string, value entryValue) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.items[username] = entry{
+		value:  value,
+		expiry: time.Now().Add(c.ttl),
+	}
+}
+
+func (c *Cache) Get(username string) (entryValue, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	item, found := c.items[username]
+	if !found {
+		return item.value, false
+	}
+
+	if item.isExpired() {
+		delete(c.items, username)
+		return item.value, false
+	}
+
+	return item.value, true
+}
+
+func (c *Cache) Remove(username string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	delete(c.items, username)
+}
+
 // returns a new ClaimsManager
 // precompiles JMESPath queries for later use
 func NewClaimsManager(persistClaims []string) ClaimsManager {
 	cm := ClaimsManager{
 		cookieName:    CookieName,
+		cache:         CreateCache(),
 		PersistClaims: []JMESPathQuery{},
 	}
 	for _, c := range persistClaims {
@@ -53,12 +135,39 @@ type Claims map[string]any
 // and providing helper functions to persist these claims via cookies
 type ClaimsManager struct {
 	cookieName    string
+	cache         *Cache
 	PersistClaims []JMESPathQuery
 }
 
 type JMESPathQuery struct {
 	path  string
 	query *jmespath.JMESPath
+}
+
+// Stores claims in cache; if an entry with the same cookie and username exists, is omitted
+// returns true if cache entry set, otherwise false (not sure if useful?)
+func (cm *ClaimsManager) SetClaims(username string, cookie string, data Claims) bool {
+	// check if username exists in cache or is cookie updated
+	values, ok := cm.cache.Get(username)
+	// add / update entry
+	if !ok || values.cookie != cookie {
+		newValues := entryValue{
+			cookie: cookie,
+			claims: data,
+		}
+		cm.cache.Set(username, newValues)
+		return true
+	}
+
+	return false
+}
+
+func (cm *ClaimsManager) GetClaims(username string) Claims {
+	values, ok := cm.cache.Get(username)
+	if !ok {
+		return Claims{}
+	}
+	return values.claims
 }
 
 // SetCookie creates and sets the cookie with claims persisted from the provider token;
@@ -167,10 +276,6 @@ func (cm *ClaimsManager) ExtractClaimsFromJWTPayload(accessToken string) Claims 
 			continue
 		}
 		extractedClaims[pc.path] = c
-	}
-
-	if len(extractedClaims) == 0 {
-		return nil
 	}
 
 	return extractedClaims
