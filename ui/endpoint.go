167ee3aeefa83a87aadea3b62e07a3e8327d193a
Trevin Chow <trevin@trevinchow.com>
2026-04-02 20:33:55 -0700

    Set specific Content-Type for static resources
    Use mime.TypeByExtension to detect MIME types from file extensions when
    serving static assets, and add X-Content-Type-Options: nosniff to prevent
    MIME confusion attacks. Previously all static files were served with a
    generic Content-Type, forcing browsers to sniff the correct type.
    
    Fixes #3979
    
    Signed-off-by: Trevin Chow <trevin@trevinchow.com>

--- ui/endpoint.go
+++ ui/endpoint.go
@@ -19,8 +19,10 @@ import (
 	"fmt"
 	"io"
 	"io/fs"
+	"mime"
 	"net/http"
 	"net/http/httputil"
+	"path/filepath"
 	"regexp"
 	"strings"
 
@@ -174,6 +176,10 @@ func (f *frontend) assetHandler() echo.HandlerFunc {
 		if strings.Contains(fileName, ".js") {
 			data = bytes.ReplaceAll(data, []byte(prefixPathPlaceholder), []byte(f.apiPrefix))
 		}
+		if ct := mime.TypeByExtension(filepath.Ext(fileName)); ct != "" {
+			c.Response().Header().Set("Content-Type", ct)
+		}
+		c.Response().Header().Set("X-Content-Type-Options", "nosniff")
 		_, err = c.Response().Write(data)
 		return apiinterface.HandleError(err)
 	}
