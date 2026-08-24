b9a243caea91f6c68b46ec507a7babafbe042f27
Guillaume <gladorme@gmail.com>
2026-08-24 18:59:56 +0000

    [BUGFIX] Add JSON import attributes to E2E fixtures
    Signed-off-by: Guillaume <gladorme@gmail.com>

--- ui/e2e/src/tests/jsonEditor.spec.ts
+++ ui/e2e/src/tests/jsonEditor.spec.ts
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import updatedDefaultsDashboard from '../data/updatedDefaultsDashboard.json';
+import updatedDefaultsDashboard from '../data/updatedDefaultsDashboard.json' with { type: 'json' };
 import { test, expect } from '../fixtures/dashboardTest';
 
 test.use({
