6c5c1e6fd920ec6ab54c9ff07358dd09b43558d1
gfierksap <gabor.fierka@sap.com>
2026-09-08 08:20:35 +0200

    DiscoveryDefault struct added in place of a bool Default
    Signed-off-by: gfierksap <gabor.fierka@sap.com>

--- pkg/model/api/config/datasourcediscovery.go
+++ pkg/model/api/config/datasourcediscovery.go
@@ -38,6 +38,12 @@ func (d HTTPDiscovery) MarshalJSON() ([]byte, error) {
 	return json.Marshal(cfg)
 }
 
+type DiscoveryDefault struct {
+	Enable      bool              `json:"enable,omitempty" yaml:"enable,omitempty"`
+	Labels      map[string]string `json:"labels,omitempty" yaml:"labels,omitempty"`
+	Annotations map[string]string `json:"annotations,omitempty" yaml:"annotations,omitempty"`
+}
+
 type KubeServiceDiscovery struct {
 	// If set to true, Perses server will discovery the service
 	Enable bool `json:"enable,omitempty" yaml:"enable,omitempty"`
@@ -73,7 +79,7 @@ type KubernetesDiscovery struct {
 	// The labels used to filter the list of resource when contacting the Kubernetes API.
 	Labels map[string]string `json:"labels,omitempty" yaml:"labels,omitempty"`
 	// Marks the discovered datasource as default
-	Default bool `json:"default,omitempty" yaml:"default,omitempty"`
+	Default DiscoveryDefault `json:"default,omitempty" yaml:"default,omitempty"`
 }
 
 func (d *KubernetesDiscovery) Verify() error {
